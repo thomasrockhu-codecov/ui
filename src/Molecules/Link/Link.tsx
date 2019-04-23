143ad8ac5ce536fbfc7f83446ced251d476fc738
shmidt-i <bahoc47@gmail.com>
2019-04-23 11:44:41 +0200

    feat: 🎸 error message for link without to prop (#94)

--- src/Molecules/Link/Link.tsx
+++ src/Molecules/Link/Link.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import styled from 'styled-components';
 import { Link as RouterLink } from 'react-router-dom';
 import { LinkComponent, Props } from './Link.types';
+import { assert } from '../../common/utils';
 
 const StyledLink = styled(RouterLink)<Props>`
   color: ${p => p.theme.color.cta};
@@ -11,13 +12,14 @@ const StyledLink = styled(RouterLink)<Props>`
   }
 `;
 
-export const Link: LinkComponent = props => (
-  <StyledLink
-    className={props.className}
-    onClick={props.onClick}
-    to={props.to}
-    target={props.target}
-  >
-    {props.children}
-  </StyledLink>
-);
+export const Link: LinkComponent = props =>
+  assert(typeof props.to !== 'undefined', 'Link: You forgot to specify `to` property') && (
+    <StyledLink
+      className={props.className}
+      onClick={props.onClick}
+      to={props.to}
+      target={props.target}
+    >
+      {props.children}
+    </StyledLink>
+  );
--- src/common/utils.ts
+++ src/common/utils.ts
@@ -1,9 +1,11 @@
-export const assert = (expression: boolean, errorMessage: string) => {
+export const assert = (expression: boolean, errorMessage: string): true => {
   if (process.env.NODE_ENV !== 'production') {
     if (!expression) {
       throw new Error(errorMessage);
     }
   }
+  // For chaining with &&
+  return true;
 };
 
 export const isUndefined = (x: any): x is undefined => typeof x === 'undefined';
