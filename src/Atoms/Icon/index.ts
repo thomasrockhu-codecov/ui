dcfd87532fc1f8967566ccb0421b3484005794d1
Jaunius Eitmantis <jaunius.eitmantis@nordnet.se>
2019-05-02 11:37:51 +0200

    feat: add globe icon

--- src/Atoms/Icon/index.ts
+++ src/Atoms/Icon/index.ts
@@ -1,9 +1,11 @@
 import { ChevronDown } from './components/ChevronDown';
 import { ChevronUp } from './components/ChevronUp';
 import { ArrowRight } from './components/ArrowRight';
+import { Globe } from './components/Globe';
 
 export default {
   ChevronDown,
   ChevronUp,
   ArrowRight,
+  Globe,
 };
