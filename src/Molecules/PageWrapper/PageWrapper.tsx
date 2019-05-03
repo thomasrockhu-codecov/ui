7d4c99de622420e0e97260f858a4ed7a173a1d9b
Mansour J. Mansour <mansour@mjmonline.se>
2019-05-03 11:24:14 +0200

    fix: add breakpoints

--- src/Molecules/PageWrapper/PageWrapper.tsx
+++ src/Molecules/PageWrapper/PageWrapper.tsx
@@ -3,20 +3,31 @@ import styled from 'styled-components';
 import { Props } from './PageWrapper.types';
 import { Box } from '../..';
 
+const OLD_GRID_PADDING = 10 * 2; // px
+const OLD_GRID_REDUCTION = 30; // px
+const OLD_GRID_COMPABILITY = OLD_GRID_PADDING + OLD_GRID_REDUCTION;
+// TODO: remove OLD_GRID_COMPABILITY when old grid is removed
+
 const Outer = styled.div<Props>`
   ${p => (p.background ? `background-color: ${p.background(p.theme)}` : '')}
 `;
 
 const Inner = styled(Box)`
   ${p => p.theme.media.greaterThan(p.theme.size.sm)} {
-    max-width: 1252px;
+    max-width: ${p => p.theme.size.sm - OLD_GRID_COMPABILITY}px;
+  }
+  ${p => p.theme.media.greaterThan(p.theme.size.md)} {
+    max-width: ${p => p.theme.size.md - OLD_GRID_COMPABILITY}px;
+  }
+  ${p => p.theme.media.greaterThan(p.theme.size.lg)} {
+    max-width: ${p => p.theme.size.lg - OLD_GRID_COMPABILITY}px;
   }
 `;
 
 export const PageWrapper: React.FC<Props> = ({ children, background }) => {
   return (
     <Outer background={background}>
-      <Inner p={0} m="0 auto" sm={{ px: 6 }}>
+      <Inner m="0 auto" p={0} sm={{ px: 2 }}>
         {children}
       </Inner>
     </Outer>
