d86c42e96d2908ee9006c7d97929397b36843c1b
Jacob Blomgren <blomgren@protonmail.com>
2019-05-07 13:17:50 +0200

    docs: add toggler for themes

--- .storybook/ThemeDecorator.tsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-import { ThemeProvider } from 'styled-components';
-import theme from '../src/theme';
-
-export const ThemeDecorator = storyFn => <ThemeProvider theme={theme}>{storyFn()}</ThemeProvider>;
