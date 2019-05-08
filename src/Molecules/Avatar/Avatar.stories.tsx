2d98dabdb7f7dfe2e57317c494660369b291d057
Mansour J. Mansour <mansour@mjmonline.se>
2019-05-08 11:11:38 +0200

    fix: minor fixes

--- src/Molecules/Avatar/Avatar.stories.tsx
+++ src/Molecules/Avatar/Avatar.stories.tsx
@@ -2,4 +2,10 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { Avatar } from '../..';
 
-storiesOf('Molecules | Avatar', module).add('Basic usage', () => <Avatar>ASK</Avatar>);
+storiesOf('Molecules | Avatar', module)
+  .add('Basic usage', () => <Avatar>ASK</Avatar>)
+  .add('With element as children', () => (
+    <Avatar>
+      <abbr title="Investeringssparkonto">ISK</abbr>
+    </Avatar>
+  ));
