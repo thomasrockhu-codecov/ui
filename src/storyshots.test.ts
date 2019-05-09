e871972e15eac100256a4cbff6798bf35de230d0
Ivan Shmidt <bahoc47@gmail.com>
2019-05-09 13:10:55 +0200

    feat: 🎸 awesome splitted snapshots

--- src/storyshots.test.ts
+++ src/storyshots.test.ts
@@ -1,8 +1,13 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
-import 'jest-styled-components';
+import initStoryshots, { multiSnapshotWithOptions } from '@storybook/addon-storyshots';
 // eslint-disable-next-line import/no-extraneous-dependencies
-import initStoryshots from '@storybook/addon-storyshots';
+import styleSheetSerializer from 'jest-styled-components/src/styleSheetSerializer';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { addSerializer } from 'jest-specific-snapshot';
+
+addSerializer(styleSheetSerializer);
 
 initStoryshots({
+  test: multiSnapshotWithOptions({}),
   storyNameRegex: /^(?!(Documentation|Experimental)).*$/,
 });
