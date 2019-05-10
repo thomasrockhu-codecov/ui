05bb9388bcad1c9a7504a482dec8fe9ad6a33379
Gustav Zander <gustav.zander@nordnet.se>
2019-05-10 07:52:45 +0200

    feat: add new sorting icons Closes issues: TP-7418

---
+++ src/Atoms/Icon/components/SortUp.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { IconBase } from '../IconBase';
+import { BaseProps } from '../IconBase.types';
+
+export const SortUp = (props: BaseProps) => {
+  return (
+    <IconBase {...props}>
+      <polygon
+        points="4 0 8 6 0 6"
+        transform="translate(4.000000, 3.000000) scale(1, -1) rotate(180.000000) translate(-4.000000, -3.000000)"
+      />
+    </IconBase>
+  );
+};
+
+SortUp.displayName = 'Icon.SortUp';
