f584514929e371aa2855d26be3cf72db11af9a3e
Jaunius Eitmantis <jaunius.eitmantis@nordnet.se>
2019-05-13 10:24:04 +0200

    feat: expand card with title props

--- src/Molecules/CardWithTitle/CardWithTitle.types.ts
+++ src/Molecules/CardWithTitle/CardWithTitle.types.ts
@@ -2,6 +2,6 @@ import { Props as CardProps } from '../../Atoms/Card/Card.types';
 
 export type Props = {
   title: React.ReactNode;
-};
+} & CardProps;
 
-export type CardWithTitleComponent = React.FunctionComponent<CardProps & Props>;
+export type CardWithTitleComponent = React.FunctionComponent<Props>;
--- src/Organisms/CardWithTabs/CardWithTabs.tsx
+++ src/Organisms/CardWithTabs/CardWithTabs.tsx
@@ -3,8 +3,8 @@ import React from 'react';
 import { Tabs, CardWithTitle } from '../..';
 import { Component } from './CardWithTabs.types';
 
-const CardWithTabs: Component = ({ title, children }) => (
-  <CardWithTitle title={title}>
+const CardWithTabs: Component = ({ children, ...rest }) => (
+  <CardWithTitle {...rest}>
     <Tabs>{children}</Tabs>
   </CardWithTitle>
 );
