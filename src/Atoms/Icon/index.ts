a06c493cb56b1cfa11712d646706ec5d42aff715
Mansour J. Mansour <mansour@mjmonline.se>
2019-05-13 16:02:28 +0200

    feat: add flag, trash, attachement and printer

--- src/Atoms/Icon/index.ts
+++ src/Atoms/Icon/index.ts
@@ -1,15 +1,25 @@
+import { ArrowRight } from './components/ArrowRight';
+import { Attachement } from './components/Attachement';
 import { ChevronDown } from './components/ChevronDown';
 import { ChevronUp } from './components/ChevronUp';
-import { ArrowRight } from './components/ArrowRight';
-import { DirectionUp } from './components/DirectionUp';
 import { DirectionDown } from './components/DirectionDown';
+import { DirectionUp } from './components/DirectionUp';
+import { Flag } from './components/Flag';
 import { Globe } from './components/Globe';
+import { Printer } from './components/Printer';
+import { Star } from './components/Star';
+import { Trash } from './components/Trash';
 
 export default {
+  ArrowRight,
+  Attachement,
   ChevronDown,
   ChevronUp,
-  DirectionUp,
   DirectionDown,
-  ArrowRight,
+  DirectionUp,
+  Flag,
   Globe,
+  Printer,
+  Star,
+  Trash,
 };
