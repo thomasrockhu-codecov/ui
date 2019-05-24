5098e0d72d47e130185b2531de6f7a99e8b1c154
Jacob Blomgren <blomgren@protonmail.com>
2019-05-24 12:38:12 +0200

    fix(Flag): only use greyBorder when necessary

--- src/Atoms/Flag/Flag.tsx
+++ src/Atoms/Flag/Flag.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
+import * as R from 'ramda';
 import flags from './flags';
 import { FlagComponent, InternalProps } from './Flag.types';
 import { assert } from '../../common/utils';
@@ -16,7 +17,7 @@ const VIEWBOX_HEIGHT = 480;
 const VIEWBOX_WIDTH = 640;
 const StyledSvg = styled(CleanSvg)<InternalProps>`
   height: ${p => p.theme.spacing.unit(p.height)}px;
-  border: 1px solid ${p => p.theme.color.flagBorder};
+  ${p => (p.grayBorder ? `border: 1px solid ${p.theme.color.flagBorder};` : '')} 
   box-sizing: border-box;
   display: ${p => (p.inline ? 'inline-block' : 'block')};
   ${p =>
@@ -32,17 +33,18 @@ const StyledSvg = styled(CleanSvg)<InternalProps>`
   overflow: hidden;
 `;
 
-export const Flag: FlagComponent = props => {
-  assert(Boolean(props.country), 'Flag: You need to supply a country code');
-  const FlagPathComponent = props.country ? flags[props.country.toLowerCase()] : null;
-  const height = props.height || 5; // units
+export const Flag: FlagComponent = ({ country, height, inline }) => {
+  assert(Boolean(country), 'Flag: You need to supply a country code');
+  const FlagPathComponent = country ? flags[country.toLowerCase()] : null;
+  const grayBorder = ['fi', 'jp', 'ru'].some(R.equals(country.toLowerCase()));
   return FlagPathComponent ? (
     <StyledSvg
-      inline={props.inline}
+      inline={inline}
       focusable="false"
       role="img"
-      height={height}
+      height={height || 5} // units
       viewBox={`0 0 ${VIEWBOX_WIDTH} ${VIEWBOX_HEIGHT}`}
+      grayBorder={grayBorder}
     >
       <FlagPathComponent />
     </StyledSvg>
--- src/Atoms/Flag/Flag.types.ts
+++ src/Atoms/Flag/Flag.types.ts
@@ -3,6 +3,7 @@ import React from 'react';
 export type InternalProps = {
   height: number;
   inline?: boolean;
+  grayBorder: boolean;
 };
 export type Props = {
   height?: number;
