23540982e9df739bb6c028a3d3649c25161155f2
Max Åberg <aaberg.max@gmail.comgit>
2019-05-27 17:05:26 +0200

    feat: exposed onTouchStart and onClick props

--- src/Molecules/CollapsibleCard/Collapsible.types.ts
+++ src/Molecules/CollapsibleCard/Collapsible.types.ts
@@ -2,6 +2,8 @@ export type CollapsibleProps = {
   title: React.ReactNode;
   collapsedInitial?: boolean;
   heading?: 'h1' | 'h2' | 'h3';
+  onTouchStart?: (e: React.TouchEvent) => void;
+  onClick?: (e: React.MouseEvent) => void;
 };
 
 export type IndicatorsProps = {
