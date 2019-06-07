40c4102303b507b2038c157e910e3e223562fd05
Ivan Shmidt <bahoc47@gmail.com>
2019-06-07 15:08:56 +0200

    feat: improved media and useMedia hook

--- src/Atoms/Media/Media.tsx
+++ src/Atoms/Media/Media.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
-import styled from 'styled-components';
+import styled, { ThemeContext } from 'styled-components';
 import R from 'ramda';
 
 import { Props } from './Media.types';
+import { Theme } from '../../theme/theme.types';
 
 const negateMedia = R.pipe(
   R.split(' '),
@@ -15,4 +16,33 @@ const StyledDiv = styled.div<{query: Props['query']}>`
         display: none;
     }
 `
-export const Media: React.FunctionComponent<Props> = props => <StyledDiv {...props} />;
+const noop = () => {};
+const useMedia = (query: string | ((t: Theme) => string)) => {
+  const theme = React.useContext(ThemeContext);
+  const mediaQuery = (typeof query === 'string' ? query : query(theme)).replace('@media ', '');
+
+  const mediaHit =
+    typeof window === 'undefined' || !window || !window.matchMedia
+      ? { matches: null, addListener: noop, removeListener: noop }
+      : window.matchMedia(mediaQuery);
+
+  const [matches, setMatches] = React.useState(mediaHit.matches);
+
+  React.useEffect(() => {
+    const listener = (x: MediaQueryListEvent) => setMatches(x.matches);
+    mediaHit.addListener(listener);
+    return () => mediaHit.removeListener(listener);
+  }, [mediaHit]);
+
+  return matches;
+};
+
+const Media: React.FunctionComponent<Props> = props => {
+  const matches = useMedia(props.query);
+  // If matches === null it means we are in SSR
+  // Show css fallback then
+  if (matches === null) return <StyledDiv {...props} />;
+  return matches ? <StyledDiv {...props} /> : null;
+};
+
+export { Media, useMedia };
