5632ad58baa310aa811c09a37df989414108093c
Ivan Shmidt <bahoc47@gmail.com>
2019-08-12 17:32:48 +0200

    fix: tests

--- jest.enzyme.js
+++ jest.enzyme.js
@@ -2,6 +2,19 @@ const Enzyme = require('enzyme');
 const Adapter = require('enzyme-adapter-react-16');
 const registerRequireContextHook = require('babel-plugin-require-context-hook/register');
 
+jest.mock('framer-motion', () => {
+  const React = require('react');
+  const Dummy = ({ children }) =>
+    React.createElement(React.Fragment, {}, ...(Array.isArray(children) ? children : [children]));
+
+  return {
+    motion: {
+      span: Dummy,
+    },
+    AnimatePresence: Dummy,
+  };
+});
+
 registerRequireContextHook();
 
 Enzyme.configure({ adapter: new Adapter() });
