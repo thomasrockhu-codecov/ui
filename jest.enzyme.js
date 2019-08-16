6571fa36c15cef8e24dc2477af1a1abc2e0b5aea
Jesper Winér <Jesper.Winer@nordnet.se>
2019-08-16 16:51:58 +0200

    feat(modal): change modal to use framer-motion

--- jest.enzyme.js
+++ jest.enzyme.js
@@ -2,6 +2,20 @@ const Enzyme = require('enzyme');
 const Adapter = require('enzyme-adapter-react-16');
 const registerRequireContextHook = require('babel-plugin-require-context-hook/register');
 
+jest.mock('framer-motion', () => {
+  const React = require('react'); // eslint-disable-line global-require
+  const Dummy = (type = React.Fragment) => ({ children }) =>
+    React.createElement(type, {}, ...(Array.isArray(children) ? children : [children]));
+
+  return {
+    motion: {
+      span: Dummy('span'),
+      div: Dummy('div'),
+    },
+    AnimatePresence: Dummy(),
+  };
+});
+
 registerRequireContextHook();
 
 Enzyme.configure({ adapter: new Adapter() });
