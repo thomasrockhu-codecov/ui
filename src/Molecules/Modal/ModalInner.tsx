8b034570ff9d03032266a89e16dee830f34bb3e7
Mansour J. Mansour <mansour@mjmonline.se>
2019-09-02 13:52:05 +0200

    fix: mobile full screen modal

--- src/Molecules/Modal/ModalInner.tsx
+++ src/Molecules/Modal/ModalInner.tsx
@@ -6,28 +6,22 @@ import { motion } from 'framer-motion';
 import { InnerProps, DialogProps } from './Modal.types';
 import NormalizedElements from '../../common/NormalizedElements';
 import { isFunction } from '../../common/utils';
-import { Box, Flexbox, Icon, Media, Typography, useKeyPress } from '../..';
+import { Flexbox, Typography, Icon, Box, useKeyPress } from '../..';
 
-const Backdrop = styled.div`
+const Backdrop = styled(Flexbox)`
+  position: fixed;
+  top: 0;
+  left: 0;
+  height: 100%;
+  width: 100%;
+  z-index: 2;
+
   ${({ theme }) => theme.media.greaterThan(theme.breakpoints.sm)} {
-    display: flex;
-    justify-content: center;
-    align-items: center;
     background-color: ${({ theme }) => theme.color.modalBackdrop};
-    position: fixed;
-    top: 0;
-    left: 0;
-    height: 100%;
-    width: 100%;
-    z-index: 2;
   }
 `;
 
-const DialogWrapper = styled(motion.div)`
-  z-index: 3;
-`;
-
-const StyledDialog = styled.div<DialogProps>`
+const Dialog = styled(motion.div)<DialogProps>`
   box-sizing: border-box;
   padding: ${({ theme }) => theme.spacing.unit(5)}px;
   border: 0;
@@ -36,6 +30,7 @@ const StyledDialog = styled.div<DialogProps>`
   ${({ theme }) => theme.media.lessThan(theme.breakpoints.sm)} {
     width: 100%;
     height: 100%;
+    transform: none !important; /* disables the appear animation */
   }
 
   ${({ theme }) => theme.media.greaterThan(theme.breakpoints.sm)} {
@@ -65,6 +60,21 @@ const Content = styled.div`
 export const ModalInner: React.FC<InnerProps> = ({ children, className, title, onClose }) => {
   const [show, setShow] = useState(false);
   const escapePress = useKeyPress('Escape');
+  const animationProps = {
+    initial: {
+      opacity: 0,
+      y: 70,
+    },
+    animate: {
+      opacity: 1,
+      y: 0,
+    },
+    transition: {
+      type: 'spring',
+      damping: 15,
+      stiffness: 200,
+    },
+  };
 
   useEffect(() => {
     setShow(true); // Show is only used for animation
@@ -78,60 +88,35 @@ export const ModalInner: React.FC<InnerProps> = ({ children, className, title, o
     onClose();
   }
 
-  const Dialog = () => (
-    <StyledDialog
-      className={className}
-      role="dialog"
-      show={show}
-      {...(title ? { 'aria-label': title } : {})} // TODO: move to aria-labeledby when SSR uid works
-    >
-      <Box mb={2}>
-        <Flexbox container alignItems="baseline">
-          {title && (
-            <Flexbox item>
-              <Typography as="h2" type="title2">
-                {title}
-              </Typography>
-            </Flexbox>
-          )}
-          <RightAlignedFlex item>
-            <Button type="button" onClick={onClose}>
-              <Icon.Cross size={5} title="Close this dialog" />
-            </Button>
-          </RightAlignedFlex>
-        </Flexbox>
-      </Box>
-      <Content>{children}</Content>
-    </StyledDialog>
-  );
-
   return (
     <>
       <FocusLock>
         <RemoveScroll>
-          <Backdrop>
-            <Media query={t => t.media.lessThan(t.breakpoints.sm)}>
-              <Dialog />
-            </Media>
-            <Media query={t => t.media.greaterThan(t.breakpoints.sm)}>
-              <DialogWrapper
-                initial={{
-                  opacity: 0,
-                  y: 70,
-                }}
-                animate={{
-                  opacity: 1,
-                  y: 0,
-                }}
-                transition={{
-                  type: 'spring',
-                  damping: 15,
-                  stiffness: 200,
-                }}
-              >
-                <Dialog />
-              </DialogWrapper>
-            </Media>
+          <Backdrop className={className} container alignItems="center" justifyContent="center">
+            <Dialog
+              role="dialog"
+              show={show}
+              {...(title ? { 'aria-label': title } : {})} // TODO: move to aria-labeledby when SSR uid works
+              {...animationProps}
+            >
+              <Box mb={2}>
+                <Flexbox container alignItems="baseline">
+                  {title && (
+                    <Flexbox item>
+                      <Typography as="h2" type="title2">
+                        {title}
+                      </Typography>
+                    </Flexbox>
+                  )}
+                  <RightAlignedFlex item>
+                    <Button type="button" onClick={onClose}>
+                      <Icon.Cross size={5} title="Close this dialog" />
+                    </Button>
+                  </RightAlignedFlex>
+                </Flexbox>
+              </Box>
+              <Content>{children}</Content>
+            </Dialog>
           </Backdrop>
         </RemoveScroll>
       </FocusLock>
