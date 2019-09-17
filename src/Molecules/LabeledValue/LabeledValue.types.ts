a498ecf0d84948e6ecdebd9da265521302a77a79
Joel Kallin <Joel.Kallin@nordnet.se>
2019-09-17 16:02:43 +0200

    fix: 🐛 remove unused type

--- src/Molecules/LabeledValue/LabeledValue.types.ts
+++ src/Molecules/LabeledValue/LabeledValue.types.ts
@@ -3,7 +3,6 @@ import React, { ReactNode } from 'react';
 export type Props = {
   label: string | React.ReactNode;
   children: ReactNode;
-  labelType?: 'tertiary';
 };
 
 export type LabeledValueComponent = React.FunctionComponent<Props>;
