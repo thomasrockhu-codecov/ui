f75be8adc95fceb61162cfebaf71ceeaf7e2ba64
Gustav Zander <gustav.zander@nordnet.se>
2019-09-17 08:04:36 +0200

    fix: 🐛 minify svg

--- src/Atoms/Icon/components/ExternalLink.tsx
+++ src/Atoms/Icon/components/ExternalLink.tsx
@@ -5,12 +5,10 @@ import { BaseProps } from '../IconBase.types';
 export const ExternalLink = (props: BaseProps) => {
   return (
     <IconBase {...props}>
-      <g stroke="none" strokeWidth="1" fillRule="evenodd">
-        <path
-          d="M6.67369399,0.417438743 L23.5768732,0.484990138 L23.644694,17.3874387 L20.999694,20.0324387 L20.9392091,5.00743874 L2.46289185,23.4846543 L0.577209052,21.5989715 L19.0532091,3.12143874 L4.03069399,3.06043874 L6.67369399,0.417438743 Z"
-          fillRule="nonzero"
-        />
-      </g>
+      <path
+        d="M6.674.417l16.903.068.068 16.902L21 20.032l-.06-15.025L2.462 23.485.577 21.599 19.053 3.121l-15.022-.06L6.674.416z"
+        fillRule="nonzero"
+      />
     </IconBase>
   );
 };
