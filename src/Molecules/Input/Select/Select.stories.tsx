e53071b916464171bf007d35ae4c6aae2f65843c
Ivan Shmidt <bahoc47@gmail.com>
2019-09-26 16:07:37 +0200

    fix: storysource

--- src/Molecules/Input/Select/Select.stories.tsx
+++ src/Molecules/Input/Select/Select.stories.tsx
@@ -168,7 +168,7 @@ storiesOf('Molecules | Input / Select', module)
       const SELECT_NAME = 'mySelect';
 
       // @ts-ignore
-      const [_, forceUpdate] = React.useState([] as any[]); // eslint-disable-line @typescript-eslint/no-unused-vars
+      const [_, forceUpdate] = React.useState<any[]>([]); // eslint-disable-line @typescript-eslint/no-unused-vars
 
       return (
         <form name={FORM_NAME}>
