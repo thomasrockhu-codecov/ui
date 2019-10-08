795c7ea9fc685c441cf5fa4b5d56558fffac6bc4
Viking Jonsson <Viking.Jonsson@nordnet.se>
2019-10-08 14:51:52 +0200

    fix: remove redundant background

--- src/theme/theme.ts
+++ src/theme/theme.ts
@@ -162,8 +162,7 @@ export const createTheme = (config: ThemeConfig = {}): Theme => {
       generationSavingsTimelineColor2: rawColor.complementaryPink1,
       generationSavingsTimelineColor3: rawColor.brandBlue,
       generationSavingsTimelineColor4: rawColor.complementaryBlue1,
-      orderDepthBackground: rawColor.white,
-      orderDepthDarkBackground: rawColor.gray6,
+      orderDepthBackground: rawColor.gray6,
     },
     media: {
       between: (s1, s2) => {
--- src/theme/theme.types.ts
+++ src/theme/theme.types.ts
@@ -135,10 +135,8 @@ export type Theme = {
     generationSavingsTimelineColor3: RawColor['brandBlue'];
     /** complementaryBlue1 */
     generationSavingsTimelineColor4: RawColor['complementaryBlue1'];
-    /** white */
-    orderDepthBackground: RawColor['white'];
     /** gray6 */
-    orderDepthDarkBackground: RawColor['gray6'];
+    orderDepthBackground: RawColor['gray6'];
   };
 
   spacing: {
