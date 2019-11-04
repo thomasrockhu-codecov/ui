0b6e23403b83522fc6ee2bfc773d0653f6fbe64a
Ivan Shmidt <bahoc47@gmail.com>
2019-11-04 16:08:32 +0100

    fix(icon.plus): viewbox

--- src/Atoms/Icon/components/Plus.tsx
+++ src/Atoms/Icon/components/Plus.tsx
@@ -4,7 +4,7 @@ import { BaseProps } from '../IconBase.types';
 
 export const Plus = (props: BaseProps) => {
   return (
-    <IconBase {...props} viewBox="0 0 13 12">
+    <IconBase {...props} viewBox="0 0 12 12">
       <path d="M7.166 0v5h5v2h-5v5h-2V6.999L.166 7V5l5-.001V0h2z" fillRule="evenodd" />
     </IconBase>
   );
