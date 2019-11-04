2e50ec25189be537ab6b067ae6477352cf20c6ef
Joel Kallin <Joel.Kallin@nordnet.se>
2019-11-04 15:33:45 +0100

    fix: 🐛 add viewbox to plus and minus icons

--- src/Atoms/Icon/components/Minus.tsx
+++ src/Atoms/Icon/components/Minus.tsx
@@ -4,7 +4,7 @@ import { BaseProps } from '../IconBase.types';
 
 export const Minus = (props: BaseProps) => {
   return (
-    <IconBase {...props}>
+    <IconBase {...props} viewBox="0 0 13 2">
       <path d="M12.166 0v2h-12V0z" fillRule="evenodd" />
     </IconBase>
   );
