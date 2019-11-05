766f2c4fe887317edf2325cc9030cfad9429eb4b
Ivan Shmidt <bahoc47@gmail.com>
2019-11-06 00:10:17 +0100

    refactor(dropdownBubble): css cleanup

--- src/Atoms/DropdownBubble/DropdownBubble.tsx
+++ src/Atoms/DropdownBubble/DropdownBubble.tsx
@@ -2,6 +2,7 @@ import styled, { css } from 'styled-components';
 import { Props } from './DropdownBubble.types';
 
 const TRIANGLE_SIZE = 6;
+const TOP_OFFSET = 10;
 
 const leftAndRightCss = css<Props>`
   ${p => {
@@ -31,14 +32,14 @@ const triangleCss = css`
   &:before {
     ${leftAndRightCss}
     ${commonTriangleCss}
-  top: -${TRIANGLE_SIZE}px;
+    bottom: 100%;
     border-bottom: ${TRIANGLE_SIZE}px solid;
     border-bottom-color: ${p => p.theme.color.bubbleBorder};
   }
   &:after {
     ${leftAndRightCss}
     ${commonTriangleCss}
-  top: -${TRIANGLE_SIZE - 1}px;
+    bottom: calc(100% - 1px);
     border-bottom: ${TRIANGLE_SIZE}px solid;
     border-bottom-color: ${p => p.theme.color.bubbleBackground};
   }
@@ -46,7 +47,7 @@ const triangleCss = css`
 
 export const DropdownBubble = styled.div<Props>`
   position: relative;
-  top: 10px;
+  top: ${TOP_OFFSET}
   border: 1px solid ${p => p.theme.color.bubbleBorder};
   background-color: ${p => p.theme.color.bubbleBackground};
   box-shadow: 0 2px 4px 0 rgba(40, 40, 35, 0.15);
