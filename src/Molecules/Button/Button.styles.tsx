d32f2aa518e8f1c6dcfa5fcc5262bd3deb53c479
Mansour J. Mansour <mansour@mjmonline.se>
2019-11-14 17:51:25 +0100

    fix: sync button styles with design

---
+++ src/Molecules/Button/Button.styles.tsx
@@ -0,0 +1,130 @@
+import { css } from 'styled-components';
+import Color from 'color';
+import { ButtonProps } from './Button.types';
+
+const HEIGHT = {
+  s: 6,
+  m: 8,
+  l: 10,
+};
+const PADDING_VERTICAL = {
+  s: 0,
+  m: 1,
+  l: 2,
+};
+const PADDING_HORIZONTAL = {
+  s: 2,
+  m: 3,
+  l: 4,
+};
+const BORDER_SIZE = 2;
+
+const getBorder = (color: string) => `
+  &::before {
+    content: '';
+    display: block;
+    position: absolute;
+    border: ${BORDER_SIZE}px solid ${color};
+    top: 0;
+    left: 0;
+    width: calc(100% - ${BORDER_SIZE * 2}px);
+    height: calc(100% - ${BORDER_SIZE * 2}px);
+  }
+`;
+
+const shared = css<ButtonProps>`
+  position: relative;
+  box-sizing: border-box;
+  align-items: center;
+  justify-content: center;
+  ${p => (p.fullWidth ? `display: flex; width: 100%;` : `display: inline-flex;`)}
+`;
+
+const minHeight = css<ButtonProps>`
+  min-height: ${p => p.theme.spacing.unit(HEIGHT[p.size!])}px;
+`;
+
+const padding = css<ButtonProps>`
+  ${p => {
+    const unit = PADDING_HORIZONTAL[p.size!];
+
+    return `
+      padding: ${p.theme.spacing.unit(PADDING_VERTICAL[p.size!])}px ${p.theme.spacing.unit(unit)}px;
+    `;
+  }}
+`;
+
+export const primaryStyles = css<ButtonProps>`
+  ${shared}
+  ${padding}
+  ${minHeight}
+  background-color: ${p => (p.disabled ? p.theme.color.disabledBackground : p.theme.color.cta)};
+  color: ${p => (p.disabled ? p.theme.color.disabledText : p.theme.color.buttonText)};
+  ${getBorder('transparent')}
+
+  ${p =>
+    !p.disabled &&
+    `
+    &:hover {
+      background-color: ${Color(p.theme.color.cta).darken(0.1)};
+    }
+    &:active {
+      background-color: ${Color(p.theme.color.cta).darken(0.2)};
+    }
+  `}
+`;
+
+export const secondaryStyles = css<ButtonProps>`
+  ${shared}
+  ${padding}
+  ${minHeight}
+  background-color: ${p =>
+    p.disabled ? p.theme.color.disabledBackground : p.theme.color.buttonSecondaryBackground};
+
+  ${p => {
+    const color = p.colorFn && p.colorFn(p.theme);
+
+    return `
+      color: ${p.disabled ? p.theme.color.disabledText : color || p.theme.color.cta};
+      ${getBorder(p.disabled ? 'transparent' : color || p.theme.color.cta)}
+
+      ${!p.disabled &&
+        `
+        &:hover {
+          color: ${color ? Color(color).darken(0.2) : Color(p.theme.color.cta).darken(0.2)};
+    
+          &::before {
+            border-color: ${
+              color ? Color(color).darken(0.2) : Color(p.theme.color.cta).darken(0.2)
+            };
+          }
+        }
+    
+        &:active {
+          color: ${color ? Color(color).darken(0.3) : Color(p.theme.color.cta).darken(0.3)};
+    
+          &::before {
+            border-color: ${
+              color ? Color(color).darken(0.3) : Color(p.theme.color.cta).darken(0.3)
+            };
+          }
+        }
+      `}
+    `;
+  }};
+`;
+
+export const neutralStyles = css<ButtonProps>`
+  ${shared}
+  ${minHeight}
+  padding: 0;
+  background-color: transparent;
+
+  ${p => {
+    const color = p.colorFn && p.colorFn(p.theme);
+
+    return `
+      color: ${color || p.theme.color.text}
+    `;
+  }};
+`;
--- src/Molecules/Button/Button.tsx
+++ src/Molecules/Button/Button.tsx
@@ -1,206 +1,62 @@
 import React, { useContext } from 'react';
 import R from 'ramda';
-import styled, { ThemedStyledProps } from 'styled-components';
-import Color from 'color';
+import styled, { ThemeContext } from 'styled-components';
 import { Link as RouterLink } from 'react-router-dom';
 import { ButtonComponent, ButtonProps } from './Button.types';
-import { Theme } from '../../theme/theme.types';
 import { assert } from '../../common/utils';
 import NormalizedElements from '../../common/NormalizedElements';
 import { Typography } from '../..';
 import TrackingContext from '../../common/tracking';
-
-const HEIGHT = {
-  s: 6,
-  m: 8,
-  l: 10,
-};
-const PADDING_VERTICAL = {
-  s: 0,
-  m: 1,
-  l: 2,
-};
-const PADDING_HORIZONTAL = {
-  s: 2,
-  m: 3,
-  l: 4,
-};
-const BORDER_SIZE = 2;
+import { neutralStyles, primaryStyles, secondaryStyles } from './Button.styles';
 
 const isPrimary = (variant: ButtonProps['variant']) => variant === 'primary';
 const isSecondary = (variant: ButtonProps['variant']) => variant === 'secondary';
 const isNeutral = (variant: ButtonProps['variant']) => variant === 'neutral';
 
-const getBackgroundColor = (props: ThemedStyledProps<ButtonProps, Theme>) => {
-  const { disabled, theme, variant, colorFn } = props;
-
-  if (isNeutral(variant)) {
-    return `background-color: transparent;`;
-  }
-
-  if (disabled) {
-    return `background-color: ${theme.color.disabledBackground};`;
-  }
-
-  if (isSecondary(variant)) {
-    if (colorFn) {
-      const color = colorFn(theme);
-      assert(
-        color === theme.color.cta || color === theme.color.negative,
-        'Button: color is incorrect, use only `t => t.color.cta` or `t => t.color.negative`',
-      );
-      return `
-      background-color: ${theme.color.buttonSecondaryBackground};
-      &:hover{
-          background-color: ${Color(color).darken(0.1)};
-          color: ${theme.color.buttonText};
-      }
-      &:active{
-          background-color: ${Color(color).darken(0.2)};
-      }
-  `;
-    }
-    return `
-        background-color: ${theme.color.buttonSecondaryBackground};
-        &:hover{
-            background-color: ${Color(theme.color.cta).darken(0.1)};
-            color: ${theme.color.buttonText};
-        }
-        &:active{
-            background-color: ${Color(theme.color.cta).darken(0.2)};
-        }
-    `;
-  }
-
-  if (colorFn) {
-    const color = colorFn(theme);
-    assert(
-      color === theme.color.cta || color === theme.color.negative,
-      'Button: color is incorrect, use only `t => t.color.cta` or `t => t.color.negative`',
-    );
-
-    return `
-    background-color: ${color};
-    &:hover{
-        background-color: ${Color(color).darken(0.1)};
-        color: ${theme.color.buttonText};
-    }
-    &:active{
-        background-color: ${Color(color).darken(0.2)};
-    }
-`;
-  }
-
-  return `
-        background-color: ${theme.color.cta};
-        &:hover{
-            background-color: ${Color(theme.color.cta).darken(0.1)};
-        }
-        &:active{
-            background-color: ${Color(theme.color.cta).darken(0.2)};
-        }
-    `;
-};
-
-const getMinHeight = (props: ThemedStyledProps<ButtonProps, Theme>) => {
-  const { theme, size = 'm' } = props;
-
-  return `min-height: ${theme.spacing.unit(HEIGHT[size])}px`;
-};
-
-const getPadding = (props: ThemedStyledProps<ButtonProps, Theme>) => {
-  const { size = 'm', variant } = props;
-
-  if (isNeutral(variant)) {
-    return `padding: 0;`;
-  }
-
-  return `
-    padding:
-      ${props.theme.spacing.unit(PADDING_VERTICAL[size])}px
-      ${props.theme.spacing.unit(PADDING_HORIZONTAL[size])}px;
-  `;
-};
-
-const getSharedStyle = (props: ThemedStyledProps<ButtonProps, Theme>) => {
-  const { theme, variant, fullWidth, colorFn, disabled } = props;
-
-  const color = colorFn && colorFn(theme);
-  const getColor = () => {
-    if (disabled) {
-      return `color: ${theme.color.disabledText};`;
-    }
-
-    if (isNeutral(variant)) {
-      return `color: ${color || theme.color.text}`;
-    }
-
-    return `color: ${isSecondary(variant) ? color || theme.color.cta : theme.color.buttonText};`;
-  };
-
-  const getBorderColor = () => {
-    if (disabled || isNeutral(variant)) {
-      return 'transparent';
-    }
-
-    return isSecondary(variant) ? color || theme.color.cta : 'transparent';
-  };
-
-  if ((isPrimary(variant) || isSecondary(variant)) && color) {
-    assert(
-      color === theme.color.cta || color === theme.color.negative,
-      'Button: color is incorrect, use only `t => t.color.cta` or `t => t.color.negative` for primary and secondary variant.',
-    );
-  }
-
-  return `
-    ${getBackgroundColor(props)}
-    ${getPadding(props)}
-    ${getMinHeight(props)}
-    ${getColor()}
-    position: relative;
-    box-sizing: border-box;
-    align-items: center;
-    justify-content: center;
-    ${fullWidth ? `display: flex; width: 100%;` : `display: inline-flex;`}
-
-    &::before {
-      content: '';
-      display: block;
-      border: ${BORDER_SIZE}px solid ${getBorderColor()};
-      position: absolute;
-      top: 0;
-      left: 0;
-      width: calc(100% - ${BORDER_SIZE * 2}px);
-      height: calc(100% - ${BORDER_SIZE * 2}px);
-    }
-  `;
-};
-
 const StyledButton = styled(NormalizedElements.Button)<ButtonProps>`
-  ${p => getSharedStyle(p)}
+  ${p => {
+    if (isSecondary(p.variant)) {
+      return secondaryStyles;
+    }
+
+    if (isNeutral(p.variant)) {
+      return neutralStyles;
+    }
+
+    return primaryStyles;
+  }}
   border: none;
   border-radius: 0;
   cursor: ${p => (p.disabled ? 'not-allowed' : 'pointer')};
 `;
 
 const CleanRouterLink = (props: ButtonProps) => (
-  <RouterLink {...R.omit(['fullWidth', 'colorFn', 'color'], props) as any} />
+  <RouterLink {...R.omit(['fullWidth', 'colorFn', 'color', 'variant'], props) as any} />
 );
+
 const StyledLink = styled(CleanRouterLink)<ButtonProps>`
-  ${p => getSharedStyle(p)}
+  ${p => {
+    if (isSecondary(p.variant)) {
+      return secondaryStyles;
+    }
+
+    if (isNeutral(p.variant)) {
+      return neutralStyles;
+    }
+
+    return primaryStyles;
+  }}
   text-decoration: none;
 `;
 
 export const Button: ButtonComponent = props => {
-  const typeIsNotPresent = typeof props.type === 'undefined';
   const {
     className,
     disabled,
     onClick,
-    size,
+    size = 'm',
     type = 'button',
-    variant,
+    variant = 'primary',
     fullWidth,
     to,
     children,
@@ -209,19 +65,36 @@ export const Button: ButtonComponent = props => {
     color,
     as,
   } = props;
+  const typeIsNotPresent = typeof type === 'undefined';
   const toAndDisabledAreNotPresentTogether = !(to && disabled);
+  const sizeSmallNotWithNeutralVariant = size === 's' && !isNeutral(variant);
   const trackContext = useContext(TrackingContext);
-
   const trackClick = (e: React.MouseEvent<Element, MouseEvent>) => {
     if (trackContext) trackContext.track('button', e, props);
     if (onClick) onClick(e);
   };
+  const theme = useContext(ThemeContext);
+
+  const colorFromTheme = color && color(theme);
+
+  if ((colorFromTheme && isPrimary(variant)) || isSecondary(variant)) {
+    assert(
+      colorFromTheme === theme.color.cta || colorFromTheme === theme.color.negative,
+      'Button: color is incorrect, use only `t => t.color.cta` or `t => t.color.negative` for primary and secondary variant.',
+    );
+  }
+
+  assert(
+    sizeSmallNotWithNeutralVariant,
+    'Button: Size small is only ment to be used together with neutral variant.',
+  );
 
   assert(
     toAndDisabledAreNotPresentTogether,
     "You're using `to` prop together with `disabled` prop. `Disabled` prop won't be propagated to the dom node, because <a> element can't be disabled",
     { level: 'warn' },
   );
+
   if (to && !disabled) {
     assert(
       typeIsNotPresent,
