d2e0fbe59b7f21b72e4f11371ff112b352df1265
Robin Engström <robin.engstrom@nordnet.se>
2019-11-25 13:16:12 +0100

    fix: 🐛 fix viewbox for hamburger icon making it centered

--- src/Atoms/Icon/components/Hamburger.tsx
+++ src/Atoms/Icon/components/Hamburger.tsx
@@ -4,7 +4,7 @@ import { BaseProps } from '../IconBase.types';
 
 export const Hamburger = (props: BaseProps) => {
   return (
-    <IconBase {...props}>
+    <IconBase {...props} viewBox="0 0 26 18">
       <path d="M0 0h26v2H0V0zm0 16h26v2H0v-2zm0-8h26v2H0V8z" fillRule="evenodd" />
     </IconBase>
   );
