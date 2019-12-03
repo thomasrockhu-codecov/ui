8d36c749c5d253c94de99cfade97a2a4fe7a41a0
Jesper Winér <Jesper.Winer@nordnet.se>
2019-12-03 11:25:21 +0100

    fix: add type to link that looks like button

--- src/Molecules/Link/Link.tsx
+++ src/Molecules/Link/Link.tsx
@@ -81,6 +81,7 @@ export const Link: LinkComponent = React.forwardRef<any, LinkProps>((props, ref)
         onClick={trackClick}
         disabled={disabled}
         as={as}
+        type="button"
       >
         {children}
       </StyledButton>
