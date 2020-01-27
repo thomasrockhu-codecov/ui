65b877c65bdd01daa685ee01bcd5e5167a434c79
Ivan Shmidt <bahoc47@gmail.com>
2020-01-27 11:23:34 +0100

    chore(storybook): add mdx story support

--- .storybook/config.ts
+++ .storybook/config.ts
@@ -24,10 +24,14 @@ addLocaleData(noLocaleData);
 addLocaleData(daLocaleData);
 addLocaleData(fiLocaleData);
 // automatically import all files ending in *.stories.tsx
-const req = require.context('../src', true, /.stories.tsx$/);
+const req = require.context('../docs', true, /.stories.mdx$/);
+const req2 = require.context('../src', true, /.stories.tsx$/);
 
 function loadStories() {
-  return req.keys().map(req);
+  return req
+    .keys()
+    .map(req)
+    .concat(req2.keys().map(req2));
 }
 
 addDecorator(withA11y);
@@ -44,5 +48,48 @@ setIntlConfig({
 });
 
 addDecorator(withIntl);
+import { create } from '@storybook/theming';
+import { createTheme } from '../src/theme';
+
+const theme = createTheme({});
+// Option defaults.
+addParameters({
+  options: {
+    theme: create({
+      base: 'light',
+
+      colorPrimary: theme.color.cta,
+      colorSecondary: theme.color.cta,
+
+      // UI
+      appBg: 'white',
+      appContentBg: theme.color.background,
+      appBorderColor: theme.color.bubbleBorder,
+      appBorderRadius: 0,
+
+      // Typography
+      fontBase: '"Open Sans", sans-serif',
+      fontCode: 'monospace',
+
+      // Text colors
+      textColor: 'black',
+      textInverseColor: 'rgba(255,255,255,0.9)',
+
+      // Toolbar default and active colors
+      barTextColor: theme.color.background,
+      barSelectedColor: theme.color.textLight,
+      barBg: theme.color.backgroundBlack,
+
+      // Form colors
+      inputBg: 'white',
+      inputBorder: 'silver',
+      inputTextColor: 'black',
+      inputBorderRadius: 4,
+
+      brandTitle: 'Nordnet UI',
+      brandUrl: 'https://nordnet.se',
+    }),
+  },
+});
 
 configure(loadStories, module);
