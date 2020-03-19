2878cee1e132b06025741a9fc4f9409b81410fa6
Vladimir Starkov <iamstarkov@gmail.com>
2020-03-19 20:39:31 +0100

    fix(InfoBar): fix TS types

--- src/Molecules/InfoBar/InfoBar.stories.tsx
+++ src/Molecules/InfoBar/InfoBar.stories.tsx
@@ -1,50 +1,102 @@
 import React from 'react';
-import { storiesOf } from '@storybook/react';
-import { BrowserRouter } from 'react-router-dom';
-import { InfoBar } from '../..';
-import { InfoBarProps } from './InfoBar.types';
 
-const WrappedInfoBar: React.FC<InfoBarProps> = ({ variant, children, onClose }) => (
-  <BrowserRouter>
-    <InfoBar onClose={onClose} variant={variant}>
-      {children}
-    </InfoBar>
-  </BrowserRouter>
+import { InfoBar } from '../..';
+import { Display } from '../../common/Display';
+
+export default {
+  title: 'Molecules | InfoBar',
+  parameters: {
+    component: InfoBar,
+  },
+};
+
+export const defaultUsage = () => (
+  <InfoBar>
+    InfoBar should be used on top of the pages to show Marketing or Customer Support communication
+    messages. Content of the InfoBar is just rendered <code>children</code>
+  </InfoBar>
 );
 
-storiesOf('Webapp Next | InfoBar', module)
-  .add('Default', () => (
-    <WrappedInfoBar>
-      We’re currently working on the new main menu. Links marked with arrow icon will redirect you
-      to the previous website experience. <a href="#nonce">Read more</a>
-    </WrappedInfoBar>
-  ))
-  .add('Default with onClose', () => (
-    <WrappedInfoBar onClose={() => alert('Clicked close')}>
-      We’re currently working on the new main menu. Links marked with arrow icon will redirect you
-      to the previous website experience. <a href="#nonce">Read more</a>
-    </WrappedInfoBar>
-  ))
-  .add('General', () => (
-    <WrappedInfoBar variant="general" onClose={() => alert('Clicked close')}>
-      We’re currently working on the new main menu. Links marked with arrow icon will redirect you
-      to the previous website experience. <a href="#nonce">Read more</a>
-    </WrappedInfoBar>
-  ))
-  .add('Warning', () => (
-    <WrappedInfoBar variant="warning" onClose={() => alert('Clicked close')}>
-      We’re currently working on the new main menu. Links marked with arrow icon will redirect you
-      to the previous website experience. <a href="#nonce">Read more</a>
-    </WrappedInfoBar>
-  ))
-  .add('Error', () => (
-    <WrappedInfoBar variant="error" onClose={() => alert('Clicked close')}>
-      The US market is currently down, we are fixing it right now. It will soon be back and running.{' '}
-      <a href="#nonce">Read more</a>
-    </WrappedInfoBar>
-  ))
-  .add('Success', () => (
-    <WrappedInfoBar variant="success" onClose={() => alert('Clicked close')}>
-      We have fixed the US market issue. It is now back to service. <a href="#nonce">Read more</a>
-    </WrappedInfoBar>
-  ));
+defaultUsage.story = {
+  name: 'Default usage',
+};
+
+export const onCloseProp = () => (
+  <Display
+    items={[
+      {
+        title: 'Without onClose (default)',
+        component: <InfoBar>Without onClose prop InfoBar cannot be dismissed</InfoBar>,
+      },
+      {
+        title: 'With onClose (alert("close"))',
+        component: (
+          <InfoBar onClose={() => alert('close')}>
+            With onClose prop InfoBar can be dismissed, but ParentComponent is in charge to react
+            and actually hide the InfoBar.
+          </InfoBar>
+        ),
+      },
+    ]}
+  />
+);
+
+onCloseProp.story = {
+  name: 'onClose prop demo',
+};
+
+export const differentVariants = () => (
+  <Display
+    items={[
+      {
+        title: 'default',
+        component: (
+          <InfoBar>
+            We’re currently working on the new main menu. Links marked with arrow icon will redirect
+            you to the previous website experience. <a href="#nonce">Read more</a>
+          </InfoBar>
+        ),
+      },
+      {
+        title: 'general',
+        component: (
+          <InfoBar variant="general">
+            We’re currently working on the new main menu. Links marked with arrow icon will redirect
+            you to the previous website experience. <a href="#nonce">Read more</a>
+          </InfoBar>
+        ),
+      },
+      {
+        title: 'warning',
+        component: (
+          <InfoBar variant="warning">
+            We’re currently working on the new main menu. Links marked with arrow icon will redirect
+            you to the previous website experience. <a href="#nonce">Read more</a>
+          </InfoBar>
+        ),
+      },
+      {
+        title: 'error',
+        component: (
+          <InfoBar variant="error">
+            The US market is currently down, we are fixing it right now. It will soon be back and
+            running. <a href="#nonce">Read more</a>
+          </InfoBar>
+        ),
+      },
+      {
+        title: 'success',
+        component: (
+          <InfoBar variant="success">
+            We have fixed the US market issue. It is now back to service.{' '}
+            <a href="#nonce">Read more</a>
+          </InfoBar>
+        ),
+      },
+    ]}
+  />
+);
+
+differentVariants.story = {
+  name: 'Different variants',
+};
