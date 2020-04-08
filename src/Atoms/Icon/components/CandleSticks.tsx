f9be042981b909ddcf2a186a03e63a68677b968b
Karam Malkon <Karam.Malkon@nordnet.se>
2020-04-08 09:23:10 +0200

    fix: 🐛 remove hardcoded colors

--- src/Atoms/Icon/components/CandleSticks.tsx
+++ src/Atoms/Icon/components/CandleSticks.tsx
@@ -6,7 +6,6 @@ export const CandleSticks = (props: BaseProps) => {
   return (
     <IconBase {...props} viewBox="0 0 16 16">
       <path
-        fill="#282823"
         fillRule="evenodd"
         d="M4 0v4h1v3H4v9H2V7H1V4h1V0h2zm5 0v8h1v5H9v3H7v-3H6V8h1V0h2zm5 0v2h1v2h-1v12h-2V4h-1V2h1V0h2z"
       />
