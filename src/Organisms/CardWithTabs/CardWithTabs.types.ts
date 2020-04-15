feb09c6326cfd3b3e7e6de0a5c45cf2b0ad94d53
Mansour J. Mansour <mansour@mjmonline.se>
2020-04-15 21:32:40 +0200

    fix: make ts work

--- src/Organisms/CardWithTabs/CardWithTabs.types.ts
+++ src/Organisms/CardWithTabs/CardWithTabs.types.ts
@@ -1,11 +1,8 @@
 import { Props as CardWithTitleProps } from '../../Molecules/CardWithTitle/CardWithTitle.types';
 import { ItemComponent, ContainerProps } from '../../Molecules/Tabs/Tabs.types';
 
-export type Props = CardWithTitleProps & {
-  initialActiveTabIndex?: ContainerProps['initialActiveTabIndex'];
-  activeTabIndex?: ContainerProps['activeTabIndex'];
-};
+export type UsedContainerProps = Pick<ContainerProps, 'initialActiveTabIndex' | 'activeTabIndex'>;
 
-export type Component = React.FC<Props> & {
+export type Component = React.FC<CardWithTitleProps & UsedContainerProps> & {
   Tab: ItemComponent;
 };
