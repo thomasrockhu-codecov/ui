cd89f75751c95d48ea9e3a4fb0f89fe367a6c618
Gustav Pettersson <gustav.pettersson@nordnet.se>
2020-05-05 19:23:05 +0200

    feat: 🎸 remove custom styling of tabs

--- src/Organisms/CardWithTabs/CardWithTabs.tsx
+++ src/Organisms/CardWithTabs/CardWithTabs.tsx
@@ -1,20 +1,15 @@
 import React from 'react';
-import styled from 'styled-components';
 import { Tabs, CardWithTitle } from '../..';
 import { Component } from './CardWithTabs.types';
-
-const StyledTabs = styled(Tabs)`
-  padding: 0 ${({ theme }) => theme.spacing.unit(5)}px;
-`;
 
 const CardWithTabs: Component & {
   Tab: typeof Tabs.Tab;
 } = ({ activeTabIndex, children, initialActiveTabIndex, ...rest }) => {
   return (
     <CardWithTitle {...rest}>
-      <StyledTabs activeTabIndex={activeTabIndex} initialActiveTabIndex={initialActiveTabIndex}>
+      <Tabs activeTabIndex={activeTabIndex} initialActiveTabIndex={initialActiveTabIndex}>
         {children}
-      </StyledTabs>
+      </Tabs>
     </CardWithTitle>
   );
 };
