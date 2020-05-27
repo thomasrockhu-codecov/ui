54cd535fe6509f2999c8b70fbbf7b4db67de530c
Jesper Annebäck <jesper.anneback@nordnet.se>
2020-05-27 10:37:30 +0200

    fix: 🐛 makes indicator optional for CollapsibleCard

--- src/Molecules/CollapsibleCard/CollapsibleCard.stories.tsx
+++ src/Molecules/CollapsibleCard/CollapsibleCard.stories.tsx
@@ -80,3 +80,21 @@ export const collapsibleWithOnClickListener = () => <ExampleWithOnClick />;
 collapsibleWithOnClickListener.story = {
   name: 'Collapsible with onClick listener',
 };
+
+export const collapsibleWithoutIndicator = () => {
+  return (
+    <CollapsibleCard title="Collapsible without indicator" noIndicator>
+      <Typography type="primary" as={styledText}>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+        labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+        laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+        voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+        cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+      </Typography>
+    </CollapsibleCard>
+  );
+};
+
+collapsibleWithoutIndicator.story = {
+  name: 'Collapsible without indicator',
+};
