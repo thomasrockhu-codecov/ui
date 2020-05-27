58d40cb2e3b8f79fc9f6db38f527aa0ed0aab519
Gustav Pettersson <gustav.pettersson@nordnet.se>
2020-05-27 14:29:35 +0200

    feat: 🎸 add padding to table row

--- src/Molecules/Table/Row/Row.tsx
+++ src/Molecules/Table/Row/Row.tsx
@@ -1,4 +1,29 @@
 import React from 'react';
-import { RowComponent } from './Row.types';
+import styled from 'styled-components';
+import { RowComponent, Size } from './Row.types';
 
-export const Row: RowComponent = ({ children }) => <div role="row">{children}</div>;
+const getPaddingSize = (size: Size) => {
+  switch (size) {
+    case 's':
+      return 0;
+    case 'm':
+      return 1;
+    case 'l':
+      return 2;
+    default:
+      return 1;
+  }
+};
+
+const StyledRow = styled('div').withConfig({
+  shouldForwardProp: prop => !['size'].includes(prop),
+})<{ size: Size }>`
+  padding-top: ${p => p.theme.spacing.unit(getPaddingSize(p.size))}px;
+  padding-bottom: ${p => p.theme.spacing.unit(getPaddingSize(p.size))}px;
+`;
+
+export const Row: RowComponent = ({ size = 'm', children }) => (
+  <StyledRow size={size} role="row">
+    {children}
+  </StyledRow>
+);
--- src/Molecules/Table/Row/Row.types.ts
+++ src/Molecules/Table/Row/Row.types.ts
@@ -1,5 +1,9 @@
 import React from 'react';
 
-type Props = {};
+export type Size = 's' | 'm' | 'l';
+
+type Props = {
+  size?: Size;
+};
 
 export type RowComponent = React.FC<Props>;
