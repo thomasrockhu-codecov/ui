4cf9f057cc17509275953ca5e3ac1542e46cbd2e
Mansour J. Mansour <mansour@mjmonline.se>
2020-05-28 18:53:58 +0200

    fix: add jsdoc to expanded prop

--- src/Molecules/AccordionItem/AccordionItem.types.ts
+++ src/Molecules/AccordionItem/AccordionItem.types.ts
@@ -2,6 +2,7 @@ type Props = {
   as?: keyof JSX.IntrinsicElements | React.ComponentType<any>;
   children?: React.ReactNode;
   className?: string;
+  /** Setting this prop makes the component controlled */
   expanded?: boolean;
   expandedInitial?: boolean;
   title: string;
