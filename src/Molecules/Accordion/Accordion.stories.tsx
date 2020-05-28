ef248f509ed992141d828b0d4a8760e273c97bae
Mansour J. Mansour <mansour@mjmonline.se>
2020-05-28 09:23:53 +0200

    feat: create accordion stories

---
+++ src/Molecules/Accordion/Accordion.stories.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import { HashRouter } from 'react-router-dom';
+import { Typography, Link } from '../..';
+import Accordion from '.';
+
+const ExampleContent = () => (
+  <Typography as="p" type="secondary" color={t => t.color.accordionText}>
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+    labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+    laboris nisi ut aliquip ex ea commodo consequat
+  </Typography>
+);
+
+const exampleFooter = (
+  <Typography as="p" type="secondary" weight="bold">
+    Did not find what you look for? <Link to="/#">Check out FAQ</Link>
+  </Typography>
+);
+
+storiesOf('Instrument | Accordion', module).add('Default', () => (
+  <HashRouter>
+    <Accordion footer={exampleFooter}>
+      <ExampleContent />
+    </Accordion>
+  </HashRouter>
+));
