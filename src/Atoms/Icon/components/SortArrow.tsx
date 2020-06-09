d36e186147da82f142c937f67cc675ea0def86b8
Fredrik Lütz <frelut@c02c95bkmd6r.pilen.nordnet.se>
2020-06-09 10:37:23 +0200

    fix: 🐛 change sort arrow in table header

--- src/Atoms/Icon/components/SortArrow.tsx
+++ src/Atoms/Icon/components/SortArrow.tsx
@@ -20,9 +20,7 @@ export const SortArrow = (props: BaseProps & Direction) => {
   return (
     <StyledIconBase {...props} viewBox="0 0 8 12">
       <path
-        id="a"
         d="M9 2v8.4l1.66-1.62L12 10.1 8 14l-4-3.9 1.34-1.32L7 10.4V2h2z"
-        fill="#282823"
         fillRule="evenodd"
         transform="matrix(1 0 0 -1 -4 14)"
       />
--- src/Molecules/Table/Header/HeaderContent/SortIcon.tsx
+++ src/Molecules/Table/Header/HeaderContent/SortIcon.tsx
@@ -2,23 +2,20 @@ import React from 'react';
 import styled from 'styled-components';
 import { Icon } from '../../../..';
 import { SortOrder } from './HeaderContent.types';
+import { SORT_ORDER_NONE } from '../../shared/constants';
 
 const StyledIconChevronDown = styled(Icon.ChevronDown)`
   margin-left: ${p => p.theme.spacing.unit(1)}px;
 `;
 
-const StyledIconThinArrow = styled(Icon.ThinArrow)`
+const StyledIconSortArrow = styled(Icon.SortArrow)`
   margin-left: ${p => p.theme.spacing.unit(1)}px;
 `;
 
-export const SortIcon: React.FC<{ sortOrder: SortOrder }> = ({ sortOrder }) => {
-  if (sortOrder === 'descending') {
-    return <StyledIconThinArrow inline direction="up" size={2} color={t => t.color.text} />;
+export const SortIcon: React.FC<{ sortOrder: NonNullable<SortOrder> }> = ({ sortOrder }) => {
+  if (sortOrder === SORT_ORDER_NONE) {
+    return <StyledIconChevronDown inline size={2} color={t => t.color.label} />;
   }
 
-  if (sortOrder === 'ascending') {
-    return <StyledIconThinArrow inline direction="down" size={2} color={t => t.color.text} />;
-  }
-
-  return <StyledIconChevronDown inline size={2} color={t => t.color.label} />;
+  return <StyledIconSortArrow inline direction={sortOrder} size={2} color={t => t.color.text} />;
 };
