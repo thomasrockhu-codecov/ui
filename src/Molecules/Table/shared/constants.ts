9571fda0d7fc61090bd621f49e8f8a3ed64caddd
David Skog <david.skog@nordnet.se>
2020-06-09 15:05:33 +0200

    fix: review comment

--- src/Molecules/Table/shared/constants.ts
+++ src/Molecules/Table/shared/constants.ts
@@ -1,6 +1,6 @@
 import { theme } from '../../..';
 
-export const DENSITY_PADDING_SMALL = theme.spacing.unit(0);
+export const DENSITY_PADDING_SMALL = 0;
 export const DENSITY_PADDING_MEDIUM = theme.spacing.unit(1);
 export const DENSITY_PADDING_LARGE = theme.spacing.unit(2);
 
