0a58de365bc00d887f0a433cc02ce628fe5475f4
Marcus Bergholm <bergholmmarcus@gmail.se>
2020-06-12 11:53:30 +0200

    fix: update viewBox for file icon

--- src/Atoms/Icon/components/File.tsx
+++ src/Atoms/Icon/components/File.tsx
@@ -4,11 +4,8 @@ import { BaseProps } from '../IconBase.types';
 
 export const File = (props: BaseProps) => {
   return (
-    <IconBase {...props}>
-      <path
-        d="M10.5819065 0L14 3.4871183V16H2V0h8.5819065zM9.5 1.5h-6v12.9743126h9V4.5h-3v-3z"
-        transform="scale(1.4) translate(-1,1)"
-      />
+    <IconBase {...props} viewBox="0 0 16 16">
+      <path d="M10.5819065 0L14 3.4871183V16H2V0h8.5819065zM9.5 1.5h-6v12.9743126h9V4.5h-3v-3z" />
     </IconBase>
   );
 };
