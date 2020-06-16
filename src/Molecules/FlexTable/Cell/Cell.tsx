41b2fa0017cf1b60e7a47d5881a332f8c61f5665
Samy Khatib <samy.khatib@nordnet.se>
2020-06-16 21:39:57 +0200

    perf: ⚡️ stop cell from rerendering on sort

--- src/Molecules/FlexTable/Cell/Cell.tsx
+++ src/Molecules/FlexTable/Cell/Cell.tsx
@@ -4,12 +4,23 @@ import * as R from 'ramda';
 import { isElement, isFunction } from '../../../common/utils';
 import { Flexbox } from '../../..';
 import { useColumn } from '../shared/ColumnProvider';
-import { CellComponent } from './Cell.types';
+import { CellComponent, InnerCellComponent } from './Cell.types';
 import { TextWrapper } from './TextWrapper';
 
 const StyledFlexbox = styled(Flexbox)`
   overflow: hidden;
 `;
+
+const InnerCell: InnerCellComponent = React.memo(
+  ({ flexProps, children, className, fontSize, columnId }) => (
+    <StyledFlexbox className={className} role="cell" {...flexProps}>
+      {isElement(children) && children}
+      {isFunction(children)
+        ? children({ fontSize, columnId })
+        : !isElement(children) && <TextWrapper fontSize={fontSize}>{children}</TextWrapper>}
+    </StyledFlexbox>
+  ),
+);
 
 const Cell: CellComponent = ({ children, className, fontSize, columnId }) => {
   const [columnState] = useColumn(columnId);
@@ -17,13 +28,16 @@ const Cell: CellComponent = ({ children, className, fontSize, columnId }) => {
   if (!R.prop('flexProps', columnState)) {
     return null;
   }
+
   return (
-    <StyledFlexbox className={className} role="cell" {...columnState.flexProps}>
-      {isElement(children) && children}
-      {isFunction(children)
-        ? children({ fontSize, columnId })
-        : !isElement(children) && <TextWrapper fontSize={fontSize}>{children}</TextWrapper>}
-    </StyledFlexbox>
+    <InnerCell
+      flexProps={columnState.flexProps}
+      className={className}
+      fontSize={fontSize}
+      columnId={columnId}
+    >
+      {children}
+    </InnerCell>
   );
 };
 
