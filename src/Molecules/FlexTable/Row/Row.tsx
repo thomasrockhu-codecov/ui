4dce3527ecd0300c5b5cb63ff2b1a0867e47132f
Oscar Sandberg <oscar.sandberg@nordnet.se>
2020-06-16 11:06:15 +0200

    feat: 🎸 make expanded into row

--- src/Molecules/FlexTable/Row/Row.tsx
+++ src/Molecules/FlexTable/Row/Row.tsx
@@ -9,24 +9,22 @@ import { useFlexTable } from '../shared/FlexTableProvider';
 import { ExpandItems } from './ExpandItems';
 
 /* the cells are padded by row gutter 1 unit (4px) */
-const StyledRow = styled('div').withConfig({
+const StyledRow = styled(Flexbox).withConfig({
   shouldForwardProp: prop =>
-    !['hideSeparator', 'expanded', 'separatorColor', 'density'].includes(prop),
+    !['hideSeparator', 'expanded', 'separatorColor', 'density', 'hoverHighlight'].includes(prop),
 })<{
   hideSeparator: boolean;
   expanded: boolean;
   separatorColor: ColorFn;
+  hoverHighlight: boolean;
+  density: Density;
 }>`
-  ${p => (!p.hideSeparator ? `border-bottom: 1px solid ${p.separatorColor(p.theme)}` : '')};
+  ${p =>
+    !p.hideSeparator && !p.expanded ? `border-bottom: 1px solid ${p.separatorColor(p.theme)}` : ''};
   padding-right: ${p => p.theme.spacing.unit(1)}px;
   padding-left: ${p => p.theme.spacing.unit(0.5)}px;
   border-left: ${p => p.theme.spacing.unit(0.5)}px solid
     ${p => (p.expanded ? p.theme.color.cta : 'transparent')};
-`;
-
-const StyledFlexbox = styled(Flexbox).withConfig({
-  shouldForwardProp: prop => !['hoverHighlight', 'expanded', 'density'].includes(prop),
-})<{ hoverHighlight: boolean; expanded: boolean; density: Density }>`
   ${p =>
     p.hoverHighlight &&
     !p.expanded &&
@@ -36,9 +34,10 @@ const StyledFlexbox = styled(Flexbox).withConfig({
 
   padding-top: ${p => getDensityPaddings(p.density)}px;
   padding-bottom: ${p => getDensityPaddings(p.density)}px;
+`;
 
-  border-left: ${p => p.theme.spacing.unit(0.5)}px solid
-    ${p => (p.expanded ? p.theme.color.cta : 'transparent')};
+const StyledExpandedRow = styled.div`
+  border-left: ${p => p.theme.spacing.unit(0.5)}px solid ${p => p.theme.color.cta};
 `;
 
 export const ExpandButton: React.FC<{ expanded: boolean; onClick: () => void }> = ({
@@ -63,30 +62,30 @@ export const Row: RowComponent = ({
 }) => {
   const { density } = useFlexTable();
   return (
-    <StyledRow
-      className={className}
-      hideSeparator={hideSeparator}
-      role="row"
-      expanded={expanded}
-      separatorColor={separatorColor}
-      {...htmlProps}
-    >
-      <StyledFlexbox
+    <>
+      <StyledRow
         container
-        gutter={2}
         alignItems="center"
         hoverHighlight={hoverHighlight}
+        className={className}
+        hideSeparator={hideSeparator}
+        role="row"
         expanded={expanded}
+        separatorColor={separatorColor}
         density={density}
+        {...htmlProps}
       >
         {children}
-      </StyledFlexbox>
+      </StyledRow>
+
       {expanded && (
-        <Box px={4} pb={2}>
-          {expandItems && <ExpandItems items={expandItems} />}
-          {expandChildren}
-        </Box>
+        <StyledExpandedRow role="row">
+          <Box px={4} pb={2} role="cell">
+            {expandItems && <ExpandItems items={expandItems} />}
+            {expandChildren}
+          </Box>
+        </StyledExpandedRow>
       )}
-    </StyledRow>
+    </>
   );
 };
