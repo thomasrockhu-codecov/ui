130331a1d2852153ce6844446ed488b50250122b
Gustav Pettersson <gustav.pettersson@nordnet.se>
2020-06-22 12:53:41 +0200

    test: 💍 add test for sticky table header

--- src/Molecules/FlexTable/FlexTable.stories.tsx
+++ src/Molecules/FlexTable/FlexTable.stories.tsx
@@ -3,7 +3,7 @@ import R from 'ramda';
 import styled from 'styled-components';
 import { number, withKnobs } from '@storybook/addon-knobs';
 import FlexTable from './FlexTable';
-import { Button, Typography, Flag, Icon, Number, Flexbox } from '../..';
+import { Button, Typography, Flag, Icon, Number, Flexbox, Separator } from '../..';
 import { SortOrder } from './Header/HeaderContent/HeaderContent.types';
 import { OnSort } from './Header/Header.types';
 import { ICON_COLUMN_DEFAULT_FLEX_PROPS } from './shared/constants';
@@ -246,6 +246,127 @@ export const BigTable = () => {
           <BigTableRow key={data.rowId} data={data} />
         ))}
       </FlexTable>
+    );
+  };
+  return <ReactComponent />;
+};
+
+export const BigTableWithoutStickyHeader = () => {
+  const ReactComponent = () => {
+    const rowsLength = number('Number of rows', 500);
+    const columnsLength = number('Number of columns', 10);
+    const [sort, setSort] = useState<any>({});
+    const tableData = useMemo(() => generateTableData(rowsLength, columnsLength), [
+      rowsLength,
+      columnsLength,
+    ]);
+    const sortedData = useMemo(() => {
+      const getValue = (rowData: any) => rowData[sort.columnId.replace('column', 'value')].value;
+      return tableData.sort((rowA, rowB) => {
+        if (sort.sortOrder === 'ascending') {
+          return getValue(rowB).localeCompare(getValue(rowA));
+        }
+
+        if (sort.sortOrder === 'descending') {
+          return getValue(rowA).localeCompare(getValue(rowB));
+        }
+
+        return 0;
+      });
+    }, [tableData, sort]);
+
+    return (
+      <FlexTable stickyHeader={false}>
+        <FlexTable.HeaderRow>
+          {[...Array(columnsLength)].map((_, index) => (
+            <FlexTable.Header
+              columnId={`column${index + 1}`}
+              sortable
+              onSort={(columnId, nextSortOrder) => {
+                setSort({ columnId, sortOrder: nextSortOrder });
+              }}
+            >
+              Header {index + 1}
+            </FlexTable.Header>
+          ))}
+        </FlexTable.HeaderRow>
+        {sortedData.map(data => (
+          <BigTableRow key={data.rowId} data={data} />
+        ))}
+      </FlexTable>
+    );
+  };
+  return <ReactComponent />;
+};
+
+export const MultipleTables = () => {
+  const ReactComponent = () => {
+    const rowsLength = number('Number of rows', 100);
+    const columnsLength = number('Number of columns', 5);
+    const [sort, setSort] = useState<any>({});
+    const tableData = useMemo(() => generateTableData(rowsLength, columnsLength), [
+      rowsLength,
+      columnsLength,
+    ]);
+    const sortedData = useMemo(() => {
+      const getValue = (rowData: any) => rowData[sort.columnId.replace('column', 'value')].value;
+      return tableData.sort((rowA, rowB) => {
+        if (sort.sortOrder === 'ascending') {
+          return getValue(rowB).localeCompare(getValue(rowA));
+        }
+
+        if (sort.sortOrder === 'descending') {
+          return getValue(rowA).localeCompare(getValue(rowB));
+        }
+
+        return 0;
+      });
+    }, [tableData, sort]);
+
+    return (
+      <>
+        <FlexTable>
+          <FlexTable.HeaderRow>
+            {[...Array(columnsLength)].map((_, index) => (
+              <FlexTable.Header
+                columnId={`column${index + 1}`}
+                sortable
+                onSort={(columnId, nextSortOrder) => {
+                  setSort({ columnId, sortOrder: nextSortOrder });
+                }}
+              >
+                Table1 Header {index + 1}
+              </FlexTable.Header>
+            ))}
+          </FlexTable.HeaderRow>
+          {sortedData.map(data => (
+            <BigTableRow key={data.rowId} data={data} />
+          ))}
+        </FlexTable>
+        <br />
+        <br />
+        <Separator />
+        <br />
+        <br />
+        <FlexTable>
+          <FlexTable.HeaderRow>
+            {[...Array(columnsLength)].map((_, index) => (
+              <FlexTable.Header
+                columnId={`column${index + 1}`}
+                sortable
+                onSort={(columnId, nextSortOrder) => {
+                  setSort({ columnId, sortOrder: nextSortOrder });
+                }}
+              >
+                Table 2 Header {index + 1}
+              </FlexTable.Header>
+            ))}
+          </FlexTable.HeaderRow>
+          {sortedData.map(data => (
+            <BigTableRow key={data.rowId} data={data} />
+          ))}
+        </FlexTable>
+      </>
     );
   };
   return <ReactComponent />;
