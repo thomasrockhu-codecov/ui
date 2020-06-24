229bc9f6f72afd758c2d6d7e46e37a2b299756c1
fredriklu <fredde_lutz@hotmail.com>
2020-06-24 09:57:16 +0200

    refactor: 💡 default tables into same story

--- src/Molecules/FlexTable/FlexTable.stories.tsx
+++ src/Molecules/FlexTable/FlexTable.stories.tsx
@@ -18,126 +18,160 @@ export default {
   },
 };
 
-export const DefaultTable = () => (
-  <FlexTable>
-    <FlexTable.HeaderRow>
-      <FlexTable.Header columnId="column1">Header 1</FlexTable.Header>
-      <FlexTable.Header columnId="column2">Header 2</FlexTable.Header>
-      <FlexTable.Header columnId="column3">Header 3</FlexTable.Header>
-    </FlexTable.HeaderRow>
-    <FlexTable.Row>
-      <FlexTable.Cell columnId="column1">Cell 1-1</FlexTable.Cell>
-      <FlexTable.Cell columnId="column2">Cell 1-2</FlexTable.Cell>
-      <FlexTable.Cell columnId="column3">Cell 1-3</FlexTable.Cell>
-    </FlexTable.Row>
-    <FlexTable.Row>
-      <FlexTable.Cell columnId="column1">Cell 2-1</FlexTable.Cell>
-      <FlexTable.Cell columnId="column2">Cell 2-2</FlexTable.Cell>
-      <FlexTable.Cell columnId="column3">Cell 2-3</FlexTable.Cell>
-    </FlexTable.Row>
-    <FlexTable.Row>
-      <FlexTable.Cell columnId="column1">Cell 3-1</FlexTable.Cell>
-      <FlexTable.Cell columnId="column2">Cell 3-2</FlexTable.Cell>
-      <FlexTable.Cell columnId="column3">Cell 3-3</FlexTable.Cell>
-    </FlexTable.Row>
-  </FlexTable>
-);
+const StyledDiv = styled.div`
+  background-color: ${p => p.theme.color.background};
+`;
 
-export const DefaultTableWithFooter = () => (
-  <FlexTable>
-    <FlexTable.HeaderRow>
-      <FlexTable.Header columnId="column1">Header 1</FlexTable.Header>
-      <FlexTable.Header columnId="column2">Header 2</FlexTable.Header>
-      <FlexTable.Header columnId="column3">Header 3</FlexTable.Header>
-    </FlexTable.HeaderRow>
-    <FlexTable.Row>
-      <FlexTable.Cell columnId="column1">Cell 1-1</FlexTable.Cell>
-      <FlexTable.Cell columnId="column2">Cell 1-2</FlexTable.Cell>
-      <FlexTable.Cell columnId="column3">Cell 1-3</FlexTable.Cell>
-    </FlexTable.Row>
-    <FlexTable.Row>
-      <FlexTable.Cell columnId="column1">Cell 2-1</FlexTable.Cell>
-      <FlexTable.Cell columnId="column2">Cell 2-2</FlexTable.Cell>
-      <FlexTable.Cell columnId="column3">Cell 2-3</FlexTable.Cell>
-    </FlexTable.Row>
-    <FlexTable.Row>
-      <FlexTable.Cell columnId="column1">Cell 3-1</FlexTable.Cell>
-      <FlexTable.Cell columnId="column2">Cell 3-2</FlexTable.Cell>
-      <FlexTable.Cell columnId="column3">Cell 3-3</FlexTable.Cell>
-    </FlexTable.Row>
-    <FlexTable.FooterRow>
-      <FlexTable.Footer columnId="column1">Footer 1</FlexTable.Footer>
-      <FlexTable.Footer columnId="column2">Footer 2</FlexTable.Footer>
-      <FlexTable.Footer columnId="column3">Footer 3</FlexTable.Footer>
-    </FlexTable.FooterRow>
-  </FlexTable>
-);
+const StyledTableDiv = styled.div`
+  &:not(:first-of-type) {
+    margin-top: ${p => p.theme.spacing.unit(10)}px;
+  }
+  background-color: white;
+`;
 
-export const DefaultTableWithTitle = () => (
-  <FlexTable title="Title">
-    <FlexTable.HeaderRow>
-      <FlexTable.Header columnId="column1">Header 1</FlexTable.Header>
-      <FlexTable.Header columnId="column2">Header 2</FlexTable.Header>
-      <FlexTable.Header columnId="column3">Header 3</FlexTable.Header>
-    </FlexTable.HeaderRow>
-    <FlexTable.Row>
-      <FlexTable.Cell columnId="column1">Cell 1-1</FlexTable.Cell>
-      <FlexTable.Cell columnId="column2">Cell 1-2</FlexTable.Cell>
-      <FlexTable.Cell columnId="column3">Cell 1-3</FlexTable.Cell>
-    </FlexTable.Row>
-    <FlexTable.Row>
-      <FlexTable.Cell columnId="column1">Cell 2-1</FlexTable.Cell>
-      <FlexTable.Cell columnId="column2">Cell 2-2</FlexTable.Cell>
-      <FlexTable.Cell columnId="column3">Cell 2-3</FlexTable.Cell>
-    </FlexTable.Row>
-    <FlexTable.Row>
-      <FlexTable.Cell columnId="column1">Cell 3-1</FlexTable.Cell>
-      <FlexTable.Cell columnId="column2">Cell 3-2</FlexTable.Cell>
-      <FlexTable.Cell columnId="column3">Cell 3-3</FlexTable.Cell>
-    </FlexTable.Row>
-  </FlexTable>
-);
+export const DefaultTable = () => {
+  const DefaultTableExample = () => (
+    <FlexTable>
+      <FlexTable.HeaderRow>
+        <FlexTable.Header columnId="column1">Header 1</FlexTable.Header>
+        <FlexTable.Header columnId="column2">Header 2</FlexTable.Header>
+        <FlexTable.Header columnId="column3">Header 3</FlexTable.Header>
+      </FlexTable.HeaderRow>
+      <FlexTable.Row>
+        <FlexTable.Cell columnId="column1">Cell 1-1</FlexTable.Cell>
+        <FlexTable.Cell columnId="column2">Cell 1-2</FlexTable.Cell>
+        <FlexTable.Cell columnId="column3">Cell 1-3</FlexTable.Cell>
+      </FlexTable.Row>
+      <FlexTable.Row>
+        <FlexTable.Cell columnId="column1">Cell 2-1</FlexTable.Cell>
+        <FlexTable.Cell columnId="column2">Cell 2-2</FlexTable.Cell>
+        <FlexTable.Cell columnId="column3">Cell 2-3</FlexTable.Cell>
+      </FlexTable.Row>
+      <FlexTable.Row>
+        <FlexTable.Cell columnId="column1">Cell 3-1</FlexTable.Cell>
+        <FlexTable.Cell columnId="column2">Cell 3-2</FlexTable.Cell>
+        <FlexTable.Cell columnId="column3">Cell 3-3</FlexTable.Cell>
+      </FlexTable.Row>
+    </FlexTable>
+  );
 
-export const DefaultTableWithIconColumn = () => (
-  <FlexTable>
-    <FlexTable.HeaderRow>
-      <FlexTable.Header columnId="column1">Header 1</FlexTable.Header>
-      <FlexTable.Header columnId="column2">Header 2</FlexTable.Header>
-      <FlexTable.Header columnId="column3">Header 3</FlexTable.Header>
-      <FlexTable.Header columnId="actionsColumn" {...ICON_COLUMN_DEFAULT_FLEX_PROPS} />
-    </FlexTable.HeaderRow>
-    <FlexTable.Row>
-      <FlexTable.Cell columnId="column1">Cell 1-1</FlexTable.Cell>
-      <FlexTable.Cell columnId="column2">Cell 1-2</FlexTable.Cell>
-      <FlexTable.Cell columnId="column3">Cell 1-3</FlexTable.Cell>
-      <FlexTable.Cell columnId="actionsColumn">
-        <Button variant="neutral">
-          <Icon.ThreeDots />
-        </Button>
-      </FlexTable.Cell>
-    </FlexTable.Row>
-    <FlexTable.Row>
-      <FlexTable.Cell columnId="column1">Cell 2-1</FlexTable.Cell>
-      <FlexTable.Cell columnId="column2">Cell 2-2</FlexTable.Cell>
-      <FlexTable.Cell columnId="column3">Cell 2-3</FlexTable.Cell>
-      <FlexTable.Cell columnId="actionsColumn">
-        <Button variant="neutral">
-          <Icon.ThreeDots />
-        </Button>
-      </FlexTable.Cell>
-    </FlexTable.Row>
-    <FlexTable.Row>
-      <FlexTable.Cell columnId="column1">Cell 3-1</FlexTable.Cell>
-      <FlexTable.Cell columnId="column2">Cell 3-2</FlexTable.Cell>
-      <FlexTable.Cell columnId="column3">Cell 3-3</FlexTable.Cell>
-      <FlexTable.Cell columnId="actionsColumn">
-        <Button variant="neutral">
-          <Icon.ThreeDots />
-        </Button>
-      </FlexTable.Cell>
-    </FlexTable.Row>
-  </FlexTable>
-);
+  const DefaultTableWithTitleExample = () => (
+    <FlexTable title="Title">
+      <FlexTable.HeaderRow>
+        <FlexTable.Header columnId="column1">Header 1</FlexTable.Header>
+        <FlexTable.Header columnId="column2">Header 2</FlexTable.Header>
+        <FlexTable.Header columnId="column3">Header 3</FlexTable.Header>
+      </FlexTable.HeaderRow>
+      <FlexTable.Row>
+        <FlexTable.Cell columnId="column1">Cell 1-1</FlexTable.Cell>
+        <FlexTable.Cell columnId="column2">Cell 1-2</FlexTable.Cell>
+        <FlexTable.Cell columnId="column3">Cell 1-3</FlexTable.Cell>
+      </FlexTable.Row>
+      <FlexTable.Row>
+        <FlexTable.Cell columnId="column1">Cell 2-1</FlexTable.Cell>
+        <FlexTable.Cell columnId="column2">Cell 2-2</FlexTable.Cell>
+        <FlexTable.Cell columnId="column3">Cell 2-3</FlexTable.Cell>
+      </FlexTable.Row>
+      <FlexTable.Row>
+        <FlexTable.Cell columnId="column1">Cell 3-1</FlexTable.Cell>
+        <FlexTable.Cell columnId="column2">Cell 3-2</FlexTable.Cell>
+        <FlexTable.Cell columnId="column3">Cell 3-3</FlexTable.Cell>
+      </FlexTable.Row>
+    </FlexTable>
+  );
+
+  const DefaultTableWithFooterExample = () => (
+    <FlexTable>
+      <FlexTable.HeaderRow>
+        <FlexTable.Header columnId="column1">Header 1</FlexTable.Header>
+        <FlexTable.Header columnId="column2">Header 2</FlexTable.Header>
+        <FlexTable.Header columnId="column3">Header 3</FlexTable.Header>
+      </FlexTable.HeaderRow>
+      <FlexTable.Row>
+        <FlexTable.Cell columnId="column1">Cell 1-1</FlexTable.Cell>
+        <FlexTable.Cell columnId="column2">Cell 1-2</FlexTable.Cell>
+        <FlexTable.Cell columnId="column3">Cell 1-3</FlexTable.Cell>
+      </FlexTable.Row>
+      <FlexTable.Row>
+        <FlexTable.Cell columnId="column1">Cell 2-1</FlexTable.Cell>
+        <FlexTable.Cell columnId="column2">Cell 2-2</FlexTable.Cell>
+        <FlexTable.Cell columnId="column3">Cell 2-3</FlexTable.Cell>
+      </FlexTable.Row>
+      <FlexTable.Row>
+        <FlexTable.Cell columnId="column1">Cell 3-1</FlexTable.Cell>
+        <FlexTable.Cell columnId="column2">Cell 3-2</FlexTable.Cell>
+        <FlexTable.Cell columnId="column3">Cell 3-3</FlexTable.Cell>
+      </FlexTable.Row>
+      <FlexTable.FooterRow>
+        <FlexTable.Footer columnId="column1">Footer 1</FlexTable.Footer>
+        <FlexTable.Footer columnId="column2">Footer 2</FlexTable.Footer>
+        <FlexTable.Footer columnId="column3">Footer 3</FlexTable.Footer>
+      </FlexTable.FooterRow>
+    </FlexTable>
+  );
+
+  const DefaultTableWithActionsColumnExample = () => (
+    <FlexTable>
+      <FlexTable.HeaderRow>
+        <FlexTable.Header columnId="column1">Header 1</FlexTable.Header>
+        <FlexTable.Header columnId="column2">Header 2</FlexTable.Header>
+        <FlexTable.Header columnId="column3">Header 3</FlexTable.Header>
+        <FlexTable.Header columnId="actionsColumn" {...ICON_COLUMN_DEFAULT_FLEX_PROPS} />
+      </FlexTable.HeaderRow>
+      <FlexTable.Row>
+        <FlexTable.Cell columnId="column1">Cell 1-1</FlexTable.Cell>
+        <FlexTable.Cell columnId="column2">Cell 1-2</FlexTable.Cell>
+        <FlexTable.Cell columnId="column3">Cell 1-3</FlexTable.Cell>
+        <FlexTable.Cell columnId="actionsColumn">
+          <Button variant="neutral">
+            <Icon.ThreeDots />
+          </Button>
+        </FlexTable.Cell>
+      </FlexTable.Row>
+      <FlexTable.Row>
+        <FlexTable.Cell columnId="column1">Cell 2-1</FlexTable.Cell>
+        <FlexTable.Cell columnId="column2">Cell 2-2</FlexTable.Cell>
+        <FlexTable.Cell columnId="column3">Cell 2-3</FlexTable.Cell>
+        <FlexTable.Cell columnId="actionsColumn">
+          <Button variant="neutral">
+            <Icon.ThreeDots />
+          </Button>
+        </FlexTable.Cell>
+      </FlexTable.Row>
+      <FlexTable.Row>
+        <FlexTable.Cell columnId="column1">Cell 3-1</FlexTable.Cell>
+        <FlexTable.Cell columnId="column2">Cell 3-2</FlexTable.Cell>
+        <FlexTable.Cell columnId="column3">Cell 3-3</FlexTable.Cell>
+        <FlexTable.Cell columnId="actionsColumn">
+          <Button variant="neutral">
+            <Icon.ThreeDots />
+          </Button>
+        </FlexTable.Cell>
+      </FlexTable.Row>
+    </FlexTable>
+  );
+
+  return (
+    <StyledDiv>
+      <StyledTableDiv>
+        <Typography type="title3">Default Table</Typography>
+        <DefaultTableExample />
+      </StyledTableDiv>
+      <StyledTableDiv>
+        <Typography type="title3">Default Table With Title</Typography>
+        <DefaultTableWithTitleExample />
+      </StyledTableDiv>
+      <StyledTableDiv>
+        <Typography type="title3">Default Table With Footer</Typography>
+        <DefaultTableWithFooterExample />
+      </StyledTableDiv>
+      <StyledTableDiv>
+        <Typography type="title3">Default Table With Actions Column</Typography>
+        <DefaultTableWithActionsColumnExample />
+      </StyledTableDiv>
+    </StyledDiv>
+  );
+};
 
 export const TableWithNonHiglightableRows = () => (
   <FlexTable>
@@ -578,10 +612,6 @@ export const TableWithoutSeparators = () => (
   </FlexTable>
 );
 
-const StyledDiv = styled.div`
-  background-color: ${p => p.theme.color.background};
-`;
-
 const StyledFlexTable = styled(FlexTable)`
   background-color: white;
   margin-bottom: ${p => p.theme.spacing.unit(5)}px;
@@ -685,13 +715,6 @@ const expandedItemsGenerator = (renderComponent = false) =>
     const value = renderComponent ? <Number value={valueText} /> : valueText.toString();
     return [...acc, { label, value }];
   }, []);
-
-const StyledTableDiv = styled.div`
-  &:not(:first-of-type) {
-    margin-top: ${p => p.theme.spacing.unit(10)}px;
-  }
-  background-color: white;
-`;
 
 export const ExpandableTable = () => {
   const expandItemsText = expandedItemsGenerator();
