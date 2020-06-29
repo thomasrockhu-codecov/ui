5c39159b4c2da00911a51c11c99e03ce779004d6
fredriklu <fredde_lutz@hotmail.com>
2020-06-29 14:13:00 +0200

    fix: 🐛 correct truncate and icon on same row as text

--- src/Molecules/FlexTable/Header/HeaderContent/HeaderContent.tsx
+++ src/Molecules/FlexTable/Header/HeaderContent/HeaderContent.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import styled from 'styled-components';
 import * as R from 'ramda';
 import { Props, UIProps } from './HeaderContent.types';
 import { TextWrapper } from './TextWrapper';
@@ -6,6 +7,15 @@ import { SORT_ORDER_NONE } from '../../shared/constants';
 import { SortIcon } from './SortIcon';
 import { SortButton } from './SortButton';
 import { useFlexTable } from '../../shared/FlexTableProvider';
+import { Flexbox } from '../../../..';
+
+const StyledFlexboxContainer = styled(Flexbox)`
+  justify-content: inherit;
+`;
+
+const StyledFlexbox = styled(Flexbox)`
+  min-width: 0;
+`;
 
 export const HeaderContent: React.FC<Props & UIProps> = ({
   sortable,
@@ -24,13 +34,19 @@ export const HeaderContent: React.FC<Props & UIProps> = ({
 
   return (
     <SortButton onClick={onSortClick}>
-      <TextWrapper
-        fontSize={fontSize}
-        sorted={!R.isNil(sortOrder) && sortOrder !== SORT_ORDER_NONE}
-      >
-        {children}
-      </TextWrapper>
-      <SortIcon sortOrder={sortOrder} />
+      <StyledFlexboxContainer container>
+        <StyledFlexbox item>
+          <TextWrapper
+            fontSize={fontSize}
+            sorted={!R.isNil(sortOrder) && sortOrder !== SORT_ORDER_NONE}
+          >
+            {children}
+          </TextWrapper>
+        </StyledFlexbox>
+        <Flexbox item>
+          <SortIcon sortOrder={sortOrder} />
+        </Flexbox>
+      </StyledFlexboxContainer>
     </SortButton>
   );
 };
