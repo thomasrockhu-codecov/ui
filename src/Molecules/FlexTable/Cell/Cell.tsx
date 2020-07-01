a32d8f97fc0f7257d45aba265bd021a690771e13
fredriklu <fredde_lutz@hotmail.com>
2020-07-01 10:16:40 +0200

    fix: 🐛 inner cell to retain export of text wrapper

--- src/Molecules/FlexTable/Cell/Cell.tsx
+++ src/Molecules/FlexTable/Cell/Cell.tsx
@@ -1,16 +1,27 @@
-import React, { ReactNode } from 'react';
+import React from 'react';
 import styled from 'styled-components';
 import * as R from 'ramda';
 import { isElement, isFunction } from '../../../common/utils';
 import { Flexbox } from '../../..';
 import { useColumnLayout } from '../shared/ColumnProvider';
-import { CellComponent } from './Cell.types';
+import { CellComponent, InnerCellComponent } from './Cell.types';
 import { TextWrapper } from './TextWrapper';
 import { useFlexTable } from '../shared/FlexTableProvider';
 
 const StyledFlexbox = styled(Flexbox)`
   overflow: hidden;
 `;
+
+const InnerCell: InnerCellComponent = React.memo(
+  ({ children, className, columnId, flexProps, fontSize }) => (
+    <StyledFlexbox className={className} role="cell" {...flexProps}>
+      {isElement(children) && children}
+      {isFunction(children)
+        ? children({ fontSize, columnId })
+        : !isElement(children) && <TextWrapper fontSize={fontSize}>{children}</TextWrapper>}
+    </StyledFlexbox>
+  ),
+);
 
 const Cell: CellComponent = ({ children, className, columnId }) => {
   const [columnLayout] = useColumnLayout(columnId);
@@ -21,15 +32,16 @@ const Cell: CellComponent = ({ children, className, columnId }) => {
   }
 
   return (
-    <StyledFlexbox className={className} role="cell" {...columnLayout.flexProps}>
-      {isElement(children) && children}
-      {isFunction(children)
-        ? children({ fontSize, columnId })
-        : !isElement(children) && <TextWrapper fontSize={fontSize}>{children}</TextWrapper>}
-    </StyledFlexbox>
+    <InnerCell
+      className={className}
+      columnId={columnId}
+      flexProps={columnLayout.flexProps}
+      fontSize={fontSize}
+    >
+      {children}
+    </InnerCell>
   );
 };
 
 Cell.TextWrapper = React.memo(TextWrapper);
-// React.memo/NamedExoticComponent prevents TextWrapper from being included in types, recast to retain type.
-export default (React.memo(Cell) as ReactNode) as CellComponent;
+export default Cell;
