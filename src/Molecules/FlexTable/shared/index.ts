bb312ee094f4e9d3e6d70e7bbb1dd1844d1eebe4
Samy Khatib <samy.khatib@nordnet.se>
2020-07-01 13:33:38 +0200

    test: update snapshots

--- src/Molecules/FlexTable/shared/index.ts
+++ src/Molecules/FlexTable/shared/index.ts
@@ -1,6 +1,5 @@
 import { CellInlineContainer } from './CellInlineContainer';
 import { ColumnProvider } from './ColumnProvider';
-import { StyledTruncateTooltip } from './StyledTruncateTooltip';
 import * as constants from './constants';
 
-export { constants, ColumnProvider, CellInlineContainer, StyledTruncateTooltip };
+export { constants, ColumnProvider, CellInlineContainer };
