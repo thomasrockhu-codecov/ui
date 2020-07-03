290b611bdf2711f0fb799274d93dc30f2a0f2909
Samy Khatib <samy.khatib@nordnet.se>
2020-07-03 09:54:46 +0200

    test: update snapshots

---
+++ src/Molecules/FlexTable/Row/components/ExpandElement.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import FlexTable from '../..';
+import { ICON_COLUMN_DEFAULT_FLEX_PROPS, COLUMN_ID_EXPAND } from '../../shared/constants';
+import { ExpandCell } from '../../Cell/ExpandCell';
+import { ExpandProps } from '../Row.types';
+
+export const ExpandElement: React.FC<
+  ExpandProps & { isContent: boolean; disabled?: boolean; setExpand: (expanded: boolean) => void }
+> = ({ isContent, expanded = false, onExpandToggle, setExpand, disabled = false }) => {
+  if (!isContent) {
+    return (
+      <FlexTable.Header
+        columnId={FlexTable.CONSTANTS.COLUMN_ID_EXPAND}
+        {...ICON_COLUMN_DEFAULT_FLEX_PROPS}
+      />
+    );
+  }
+
+  return (
+    <ExpandCell
+      columnId={COLUMN_ID_EXPAND}
+      expanded={expanded}
+      onClick={() => (onExpandToggle ? onExpandToggle(!expanded) : setExpand(!expanded))}
+      disabled={disabled}
+    />
+  );
+};
