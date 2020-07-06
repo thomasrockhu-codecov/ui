017f8da223118ae2cbe8678e59d5b3bf98728e97
John Lindahl <john@johnphoto.se>
2020-07-06 15:47:03 +0200

    fix: innerref needs to be specifically handled in defaultlink

--- src/common/Links/context.tsx
+++ src/common/Links/context.tsx
@@ -1,9 +1,9 @@
 import React, { createContext, FC } from 'react';
 import { LinkProps } from './types';
 
-export const DefaultLink: FC<LinkProps> = ({ to, children, ...rest }) => {
+export const DefaultLink: FC<LinkProps> = ({ to, children, innerRef, ...rest }) => {
   return (
-    <a {...rest} href={to}>
+    <a ref={innerRef} {...rest} href={to}>
       {children}
     </a>
   );
