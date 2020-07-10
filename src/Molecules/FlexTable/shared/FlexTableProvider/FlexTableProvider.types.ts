1d90eeace4ae754a4c464191014bce9158ac9abc
fredriklu <fredde_lutz@hotmail.com>
2020-07-10 10:01:15 +0200

    docs: ✏️ add jsdoc for flex table provider

--- src/Molecules/FlexTable/shared/FlexTableProvider/FlexTableProvider.types.ts
+++ src/Molecules/FlexTable/shared/FlexTableProvider/FlexTableProvider.types.ts
@@ -1,9 +1,25 @@
 import { Density, FontSize, MediaRelatedProps } from '../shared.types';
 
 type FlexTableState = {
+  /**
+   * Provides vertical padding for rows.
+   * @default 'm'
+   */
   density: Density;
+  /**
+   * Boolean to set if header is fixed or follows scroll.
+   * @default true
+   */
   stickyHeader: boolean;
+  /**
+   * Font size for the table
+   * @default 'm'
+   */
   fontSize: FontSize;
+  /**
+   * Sets table expandability. Adds empty `Header` for `HeaderRow` and chevrons for that column. Adds empty `Footer` if table contains `FooterRow`.
+   * @default false
+   */
   expandable: boolean;
 };
 
