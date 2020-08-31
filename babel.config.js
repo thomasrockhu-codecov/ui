1e80cafef125ed9ab2e9e069fa0f005312f9f5f9
Gustav Pettersson <gustav.pettersson@nordnet.se>
2020-08-31 22:04:43 +0200

    fix: 🐛 add babel plugin for optional-chaining

--- babel.config.js
+++ babel.config.js
@@ -15,6 +15,7 @@ module.exports = api => {
   ];
 
   const plugins = [
+    '@babel/plugin-proposal-optional-chaining',
     '@babel/plugin-proposal-class-properties',
     'babel-plugin-styled-components',
     'ramda',
