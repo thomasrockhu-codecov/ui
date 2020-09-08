ab2502bab1c6a2770ebaebc4045faf0a1048ec08
LifeOfMichal <dzienis.michal.maciej@gmail.com>
2020-09-08 13:43:36 +0200

    feat: 🎸 new thin icon variants for mortgage application added

---
+++ src/Atoms/Icon/components/CheckMarkCircleThin.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import styled from 'styled-components';
+import { IconBase, getColor } from '../IconBase';
+import { ChildProps, StyledChildProps } from '../IconBase.types';
+
+const StyledPolygon = styled.polygon<StyledChildProps>`
+  ${(p) => {
+    const strokeColor = getColor(p.theme, p.theme.color.svgStokeLight, p.strokeColorFn);
+    return `fill: ${strokeColor};`;
+  }}
+`;
+
+export const CheckMarkCircleThin = ({ stroke, ...props }: ChildProps) => {
+  return (
+    <IconBase {...props} viewBox="0 0 92 92">
+      <path d="M46 91.5c25.4050985 0 46-20.3710439 46-45.5S71.4050985.5 46 .5 0 20.8710439 0 46s20.5949015 45.5 46 45.5z" />
+      <path
+        fill="#FFF"
+        d="M63.1517377 34l3.573537 3.5568749L42.1676463 62l-14.870943-14.8016051 3.573537-3.5568749 11.297406 11.2447302z"
+      />
+      <StyledPolygon
+        strokeColorFn={stroke}
+        points="13.695 6 15 7.314 8.696 13.69 5 9.924 6.319 8.656 8.696 11.059"
+      />
+    </IconBase>
+  );
+};
+
+CheckMarkCircleThin.displayName = 'Icon.CheckMarkCircleThin';
---
+++ src/Atoms/Icon/components/WarningTriangleThin.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { IconBase } from '../IconBase';
+import { ChildProps } from '../IconBase.types';
+
+export const WarningTriangleThin = ({ ...props }: ChildProps) => {
+  return (
+    <IconBase {...props} viewBox="0 0 92 90">
+      <path d="M46 0l46 89.3913043H0z" />
+      <path d="M50 63.8333333v8h-8v-8h8zm0-28v24h-8v-24h8z" fill="#FFF" />
+    </IconBase>
+  );
+};
+
+WarningTriangleThin.displayName = 'Icon.WarningTriangleThin';
