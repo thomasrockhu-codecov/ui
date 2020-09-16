8f556fece9f14387914000fd41030d8f072bc060
Marcus Bergholm <bergholmmarcus@gmail.se>
2020-09-16 09:25:40 +0200

    fix: update story

--- src/Molecules/Modal/Modal.stories.tsx
+++ src/Molecules/Modal/Modal.stories.tsx
@@ -431,28 +431,9 @@ export const notFullScreenMobile = () => {
         <Modal onClose={onClose} title="Dialog information" open={open} fullScreenMobile={false}>
           <Box mb={2}>
             <Typography type="primary" as="p">
-              Modals should be used with care as they are quite intrusive on the user experience and
-              demand immediate attention (while also blocking all other actions on the site). Always
-              consider if you can solve a problem in another way first before you choose to go with
-              the modal.
+              Resize the window to see the result of setting fullScreenMobile to false.
             </Typography>
           </Box>
-          <Box mb={2}>
-            <Typography type="primary" as="p">
-              That being said they are a good tool if you need to grab the users attention, either
-              to communicate something very important or make them take an action before proceeding.
-            </Typography>
-          </Box>
-          <Box mb={2}>
-            <Typography type="primary" as="p">
-              Nielsen/Norman has an excellent article about their usage here
-            </Typography>
-          </Box>
-          <Typography type="primary" as="p">
-            <a href="https:// www.nngroup.com/articles/modal-nonmodal-dialog/">
-              https:// www.nngroup.com/articles/modal-nonmodal-dialog/
-            </a>
-          </Typography>
         </Modal>
       </div>
     );
@@ -461,5 +442,5 @@ export const notFullScreenMobile = () => {
 };
 
 notFullScreenMobile.story = {
-  name: 'Not full width mobile',
+  name: 'Not full screen mobile',
 };
