18d0a236b47dfdf20bbc6b35f4626c9904ad9c5f
Marcus Hogler <marcus.hogler@nordnet.se>
2020-09-23 10:22:00 +0200

    feat: 🎸 add stories to pagination

--- src/Molecules/Pagination/Pagination.stories.tsx
+++ src/Molecules/Pagination/Pagination.stories.tsx
@@ -18,21 +18,58 @@ export const documentation = () => (
   </Typography>
 );
 
-const PaginationWrapper = () => {
+const PaginationWrapper = ({ totalItems = 10, itemsPerPage = 1, compact = false }) => {
   const [currentPage, setCurrentPage] = React.useState(1);
 
   return (
     <Pagination
       currentPage={currentPage}
-      totalItems={999}
-      itemsPerPage={1}
+      totalItems={totalItems}
+      itemsPerPage={itemsPerPage}
       onPageChange={setCurrentPage}
+      compact={compact}
     />
   );
 };
 
-export const pagination = () => <PaginationWrapper />;
+export const defaultPagination = () => <PaginationWrapper />;
 
-pagination.story = {
-  name: 'Pagination',
+defaultPagination.story = {
+  name: 'Regular',
 };
+
+export const paginationWithOnePage = () => <PaginationWrapper totalItems={1} itemsPerPage={1} />;
+
+paginationWithOnePage.story = {
+  name: '1 page',
+};
+
+export const paginationWithTwoPages = () => <PaginationWrapper totalItems={2} itemsPerPage={1} />;
+
+paginationWithTwoPages.story = {
+  name: '2 pages',
+};
+
+export const paginationWithTwelvePages = () => (
+  <PaginationWrapper totalItems={12} itemsPerPage={1} />
+);
+
+paginationWithTwelvePages.story = {
+  name: '12 pages',
+};
+
+export const paginationWithManyPages = () => (
+  <PaginationWrapper totalItems={999} itemsPerPage={1} />
+);
+
+paginationWithManyPages.story = {
+  name: '999 pages',
+};
+
+// export const paginationCompactMobile = () => (
+//   <PaginationWrapper totalItems={10} itemsPerPage={1} compact />
+// );
+
+// paginationCompactMobile.story = {
+//   name: 'Compact on mobile',
+// };
