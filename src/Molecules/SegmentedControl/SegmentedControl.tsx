62b5390cfc058b647a7006cb075f04d62ecd2198
Jesper Winér <Jesper.Winer@nordnet.se>
2020-09-24 14:08:52 +0200

    fix: add better object check

--- src/Molecules/SegmentedControl/SegmentedControl.tsx
+++ src/Molecules/SegmentedControl/SegmentedControl.tsx
@@ -60,7 +60,11 @@ const isItemOrUndefined = (x: any): x is { type: typeof Item; props: ItemProps }
   if (x == null || typeof x === 'undefined') {
     return true;
   }
-  return typeof x === 'object' && Object.hasOwnProperty.call(x, 'type'); // FIXME: && x.type === Item;
+  return (
+    typeof x === 'object' &&
+    Object.hasOwnProperty.call(x, 'type') &&
+    x.type.displayName === Item.displayName
+  );
 };
 
 const SegmentedControl: SegmentedControlComponent = ({
