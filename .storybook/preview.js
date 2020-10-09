136bc7a096274d5b9393424c867c04188bf193b7
Tobias Lundell <lundell.tobias@gmail.com>
2020-10-09 16:26:49 +0200

    fix: 🐛 update storyshots & add missing a11y & intl decorators

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,7 +1,19 @@
 import { ThemeDecorator } from "./ThemeDecorator"
+import { withA11y } from '@storybook/addon-a11y';
+import { setIntlConfig, withIntl } from 'storybook-addon-intl';
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
 }
 
-export const decorators = [ThemeDecorator];
+setIntlConfig({
+  locales: ['sv', 'nb', 'da', 'fi', 'en'],
+  defaultLocale: 'en',
+  getMessages: () => {},
+  getFormats: () => {},
+  // Solves problem with snapshots for time component
+  timeZone: 'UTC',
+});
+
+
+export const decorators = [ThemeDecorator, withIntl, withA11y];
