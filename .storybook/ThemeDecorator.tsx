eca55a2fdcf1734826da27be5fd1fa5b08f30af5
David Skog <david.skog@nordnet.se>
2020-10-14 10:17:24 +0200

    fix: bump dependencies, fix ts problems, add missing dependencies

--- .storybook/ThemeDecorator.tsx
+++ .storybook/ThemeDecorator.tsx
@@ -1,10 +1,15 @@
-import React from 'react';
+import React, { ReactNode } from 'react';
+import { StoryFn } from '@storybook/addons';
 import { ThemeProvider } from 'styled-components';
 import { createTheme } from '../src';
 import { DocsContainer } from '@storybook/addon-docs/blocks';
 
 const theme = createTheme();
-export const ThemeDecorator = storyFn => <ThemeProvider theme={theme}>{storyFn()}</ThemeProvider>;
+
+export const ThemeDecorator = (storyFn: StoryFn<ReactNode>) => (
+  <ThemeProvider theme={theme}>{storyFn()}</ThemeProvider>
+);
+
 export const DocsWrapper = ({ children, ...rest }: any) => (
   <DocsContainer {...rest}>
     <ThemeProvider theme={theme}>{children}</ThemeProvider>
