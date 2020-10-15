f27aeafbcc3e13c51cdcc9100f451569dfa52615
Robin Engström <robin.engstrom@nordnet.se>
2020-10-15 10:49:44 +0200

    feat: 🎸 add maxWidth prop to Tooltip

--- src/Molecules/Tooltip/Tooltip.types.ts
+++ src/Molecules/Tooltip/Tooltip.types.ts
@@ -9,6 +9,8 @@ export type Props = {
   position?: 'top' | 'right' | 'bottom' | 'left';
   /** Adjusts z-index when used inside Modal */
   inModal?: boolean;
+  /** max-width in units */
+  maxWidth?: number;
 } & ReachProps;
 
 export type TooltipComponent = React.FC<Props>;
