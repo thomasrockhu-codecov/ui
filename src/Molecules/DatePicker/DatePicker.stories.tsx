17b9c3169cd239125a04c200a34e8de3dcf385b3
fredriklu <fredde_lutz@hotmail.com>
2020-11-16 11:41:52 +0100

    docs: ✏️ refactor regular + create new range date picker stories
    Authors: Robin Andersson, Fredrik Lütz

--- src/Molecules/DatePicker/DatePicker.stories.tsx
+++ src/Molecules/DatePicker/DatePicker.stories.tsx
@@ -1,8 +1,7 @@
 import React from 'react';
 import { action } from '@storybook/addon-actions';
-import isSameMonth from 'date-fns/isSameMonth';
+import { isSameWeek } from 'date-fns';
 import { DatePicker } from './DatePicker';
-import { Flexbox } from '../..';
 
 export default {
   title: 'Molecules / DatePicker',
@@ -13,65 +12,32 @@ export default {
 
 const dateNow = new Date();
 
-export const defaultStory = () => {
+export const Default = () => {
   return <DatePicker id="input-id" label="Label" onChange={action('onChange')} />;
 };
 
-defaultStory.story = {
-  name: 'Default',
-};
-
-export const disableDates = () => {
+export const SameWeekDisabled = () => {
   return (
     <DatePicker
       id="disable-dates-input"
       label="Label"
-      disableDate={(date) => !isSameMonth(dateNow, date)}
+      disableDate={(date) => isSameWeek(dateNow, date)}
       onChange={action('onChange')}
     />
   );
 };
 
-disableDates.story = {
-  name: 'Disable certain dates',
-};
-
-export const enableDates = () => {
+export const SameWeekEnabled = () => {
   return (
     <DatePicker
       id="enable-dates-input"
       label="Label"
-      enableDate={(date) => isSameMonth(dateNow, date)}
+      enableDate={(date) => isSameWeek(dateNow, date)}
       onChange={action('onChange')}
     />
   );
 };
 
-enableDates.story = {
-  name: 'Enable certain dates',
-};
-
-export const disabledInput = () => {
+export const DisabledInput = () => {
   return <DatePicker id="disabled-input" label="Label" disabled />;
-};
-
-disabledInput.story = {
-  name: 'Disabled input',
-};
-
-export const sideBySideStory = () => {
-  return (
-    <Flexbox container>
-      <Flexbox item>
-        <DatePicker id="input-id-side-one" label="Label" onChange={action('onChange')} />
-      </Flexbox>
-      <Flexbox item>
-        <DatePicker id="input-id-side-two" label="Label" onChange={action('onChange')} />
-      </Flexbox>
-    </Flexbox>
-  );
-};
-
-sideBySideStory.story = {
-  name: 'Side by side',
 };
--- src/Molecules/DatePicker/DatePickerRange.stories.tsx
+++ src/Molecules/DatePicker/DatePickerRange.stories.tsx
@@ -1,4 +1,5 @@
 import { action } from '@storybook/addon-actions';
+import { isSameWeek } from 'date-fns';
 import React from 'react';
 import DatePicker from '.';
 import { RANGE_DATE_PICKER } from './shared/constants';
@@ -10,11 +11,43 @@ export default {
   },
 };
 
+const dateNow = new Date();
+
 export const Default = () => (
   <DatePicker
-    variant={RANGE_DATE_PICKER}
     id="input-id"
+    variant={RANGE_DATE_PICKER}
     label="Label"
     onChange={action('Range date')}
   />
 );
+
+export const SameWeekDisabled = () => {
+  return (
+    <DatePicker
+      id="disable-dates-input"
+      variant={RANGE_DATE_PICKER}
+      label="Disabled dates on same week"
+      disableDate={(date) => isSameWeek(dateNow, date)}
+      onChange={action('onChange')}
+    />
+  );
+};
+
+export const SameWeekEnabled = () => {
+  return (
+    <DatePicker
+      id="enable-dates-input"
+      variant={RANGE_DATE_PICKER}
+      label="Only enabled dates in same week"
+      enableDate={(date) => isSameWeek(dateNow, date)}
+      onChange={action('onChange')}
+    />
+  );
+};
+
+export const DisabledInput = () => {
+  return (
+    <DatePicker id="disabled-input" variant={RANGE_DATE_PICKER} label="Disabled input" disabled />
+  );
+};
