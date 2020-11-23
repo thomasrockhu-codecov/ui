e6ce94380ab137fcdd13f5c3a6770f948ad5f582
Gustav Pettersson <gustav.pettersson@nordnet.se>
2020-11-23 16:31:13 +0100

    refactor: 💡 remove ununsed component renderForSizes

--- src/Molecules/FlexTable/shared/index.ts
+++ src/Molecules/FlexTable/shared/index.ts
@@ -1,6 +1,5 @@
 import { CellInlineContainer } from './CellInlineContainer';
 import { ColumnProvider } from './ColumnProvider';
-import { RenderForSizes } from './RenderForSizes';
 import * as constants from './constants';
 import { StyledTruncateTooltip } from './StyledTruncateTooltip';
 import getStylesForSizes from './getStylesForSizes';
@@ -9,7 +8,6 @@ import { Text } from './Text';
 export {
   constants,
   ColumnProvider,
-  RenderForSizes,
   CellInlineContainer,
   StyledTruncateTooltip,
   getStylesForSizes,
