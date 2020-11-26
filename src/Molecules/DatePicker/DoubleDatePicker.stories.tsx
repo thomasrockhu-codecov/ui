bcb7be5fa735499769a9a94c2c865d02c49f1305
Marcus Hogler <marcus.hogler@nordnet.se>
2020-11-26 11:38:51 +0100

    test: 💍 add disabled dates story
    authors: william bruce & marcus hogler

--- src/Molecules/DatePicker/DoubleDatePicker.stories.tsx
+++ src/Molecules/DatePicker/DoubleDatePicker.stories.tsx
@@ -1,4 +1,5 @@
 import { action } from '@storybook/addon-actions';
+import { isSameWeek } from 'date-fns';
 import React from 'react';
 import { DoubleDatePicker } from './DoubleDatePicker';
 
@@ -9,6 +10,19 @@ export default {
   },
 };
 
+const dateNow = new Date();
+
 export const Default = () => (
   <DoubleDatePicker id="input-id" label="Label" onChange={action('Range date')} />
 );
+
+export const SameWeekDisabled = () => {
+  return (
+    <DoubleDatePicker
+      id="disable-dates-input"
+      label="Disabled dates on same week"
+      disableDate={(date) => isSameWeek(dateNow, date)}
+      onChange={action('onChange')}
+    />
+  );
+};
