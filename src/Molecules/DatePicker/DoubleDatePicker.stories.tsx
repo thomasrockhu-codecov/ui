0f78575bf9f3aef4aa10441a7cb31f29aad23159
Marcus Hogler <marcus.hogler@nordnet.se>
2020-11-27 10:09:21 +0100

    test: 💍 add stories to double date picker
    authors: william bruce and marcus hogler

--- src/Molecules/DatePicker/DoubleDatePicker.stories.tsx
+++ src/Molecules/DatePicker/DoubleDatePicker.stories.tsx
@@ -1,6 +1,7 @@
 import { action } from '@storybook/addon-actions';
-import { isSameWeek } from 'date-fns';
-import React from 'react';
+import { add, isSameWeek } from 'date-fns';
+import React, { useState } from 'react';
+import { Button, Flexbox } from '../..';
 import { DoubleDatePicker } from './DoubleDatePicker';
 
 export default {
@@ -26,3 +27,56 @@ export const SameWeekDisabled = () => {
     />
   );
 };
+
+export const SameWeekEnabled = () => {
+  return (
+    <DoubleDatePicker
+      id="enable-dates-input"
+      label="Only enabled dates in same week"
+      enableDate={(date) => isSameWeek(dateNow, date)}
+      onChange={action('onChange')}
+    />
+  );
+};
+
+export const Controlled = () => {
+  const [startDate, setStartDate] = useState(new Date());
+  const [endDate, setEndDate] = useState(add(startDate, { days: 4 }));
+
+  return (
+    <>
+      <Flexbox container gutter={2}>
+        <Flexbox item>
+          <Button onClick={() => setStartDate(add(startDate, { days: 1 }))}>Next Date</Button>
+        </Flexbox>
+        <Flexbox item>
+          <Button onClick={() => setEndDate(add(endDate, { days: 1 }))}>Next End Date</Button>
+        </Flexbox>
+      </Flexbox>
+      <DoubleDatePicker
+        id="controlled"
+        label="Controlled"
+        selectedStartDate={startDate}
+        selectedEndDate={endDate}
+        onChange={(selectedStartDate, selectedEndDate) => {
+          if (selectedStartDate) setStartDate(selectedStartDate);
+          if (selectedEndDate) setEndDate(selectedEndDate);
+          action('onChange');
+        }}
+      />
+    </>
+  );
+};
+
+export const DisabledInput = () => {
+  return <DoubleDatePicker id="disabled-input" label="Disabled input" disabled />;
+};
+
+export const DisallowOneDayRange = () => (
+  <DoubleDatePicker
+    id="input-id"
+    label="Label"
+    onChange={action('Range date')}
+    disallowOneDayRange
+  />
+);
