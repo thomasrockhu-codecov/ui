58b1dfcc091ce3a85302f2c1d1b0b9cb692a3aab
Igor Smurawa <igor.smurawa@nordnet.se>
2020-12-10 10:34:12 +0100

    fix: style arrow

--- src/Molecules/TooltipNew/Tooltip.tsx
+++ src/Molecules/TooltipNew/Tooltip.tsx
@@ -6,6 +6,10 @@ import { Typography } from '../..';
 import { BORDER_SIZE } from './consts';
 import RootPortal from './lib/RootPortal';
 
+const StyledArrow = styled.span`
+  padding: 20px;
+`;
+
 const StyledTooltip = styled.span<any>`
   z-index: ${(p) => (p.inModal ? p.theme.zIndex.overlayInModal : p.theme.zIndex.overlay)};
   pointer-events: none;
@@ -16,6 +20,25 @@ const StyledTooltip = styled.span<any>`
   border: solid ${BORDER_SIZE}px ${(p) => p.theme.color.bubbleBorder};
   max-width: ${(p) => p.theme.spacing.unit(p.maxWidth)}px;
   word-break: break-all;
+
+  &[data-popper-placement^='top'] > ${StyledArrow} {
+    bottom: -4px;
+    background: red;
+  }
+  
+  &[data-popper-placement^='bottom'] > ${StyledArrow}{
+    top: -4px;
+    background: blue;
+  }
+  
+  &[data-popper-placement^='left'] > ${StyledArrow}{
+    right: -4px;
+  }
+  
+  &[data-popper-placement^='right'] > ${StyledArrow} {
+    left: -4px;
+  }
+
 `;
 
 export const Tooltip: TooltipComponent = ({
@@ -28,8 +51,11 @@ export const Tooltip: TooltipComponent = ({
 }) => {
   const [referenceElement, setReferenceElement] = useState(null);
   const [popperElement, setPopperElement] = useState(null);
+  const [arrowElement, setArrowElement] = useState(null);
 
-  const { styles, attributes } = usePopper(referenceElement, popperElement);
+  const { styles, attributes } = usePopper(referenceElement, popperElement, {
+    modifiers: [{ name: 'arrow', options: { element: arrowElement } }],
+  });
 
   return (
     <>
@@ -47,6 +73,7 @@ export const Tooltip: TooltipComponent = ({
           style={styles.popper}
           {...attributes.popper}
         >
+           <StyledArrow ref={setArrowElement} style={styles.arrow} />
           <Typography type="tertiary">{label}</Typography>
         </StyledTooltip>
       </RootPortal>
