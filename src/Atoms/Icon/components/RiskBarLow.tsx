5c26b5e9867522d9eb1534b17475e6f25f1639ec
Jesper Alvelid <jesper.alvelid@nordnet.se>
2021-04-21 08:37:11 +0200

    feat: 🎸 add new icons for etp screener
    book, bar chart style risk

---
+++ src/Atoms/Icon/components/RiskBarLow.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { IconBase } from '../IconBase';
+import { BaseProps } from '../IconBase.types';
+
+export const RiskBarLow = (props: BaseProps) => {
+  return (
+    <IconBase {...props} viewBox="0 0 16 16">
+      <g fill="none" fillRule="evenodd">
+        <rect width="3" height="11" x="7" y="5" fill="#EBEBE8" />
+        <rect width="3" height="7" x="2" y="9" fill="#FFCF00" />
+        <rect width="3" height="16" x="12" fill="#EBEBE8" />
+      </g>
+    </IconBase>
+  );
+};
+
+RiskBarLow.displayName = 'Icon.RiskBarLow';
