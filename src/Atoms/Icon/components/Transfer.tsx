d78b882295742ff1cce6b0eb57963cf8a1baa9a2
Navid Nayeri <navid.nayeri@nordnet.se>
2021-04-28 14:37:44 +0200

    fix: 🐛 Element id clash in Icons
    Same id was used for the mask element in the Icons Transfer and Percent

--- src/Atoms/Icon/components/Transfer.tsx
+++ src/Atoms/Icon/components/Transfer.tsx
@@ -6,7 +6,7 @@ import StyledPath from '../StyledPath';
 export const Transfer = ({ fill, ...props }: BaseProps & { fill: ColorFn | string }) => (
   <IconBase {...props} viewBox="0 0 32 32" fill={(t) => t.color.spinnerWhite}>
     <g transform="translate(2 5)">
-      <mask id="mask-2" fill="#fff">
+      <mask id="transfer-mask" fill="#fff">
         <path d="M28 12v10H0V12h28zm0-12v10H0V0h28z" />
       </mask>
       <StyledPath
@@ -14,7 +14,7 @@ export const Transfer = ({ fill, ...props }: BaseProps & { fill: ColorFn | strin
         strokeColorFn={fill}
         strokeWidth="2"
         d="M3 5h20M8.707 11.364L2.343 5l6.364-6.364M25 17H5m14.293 6.364L25.657 17l-6.364-6.364"
-        mask="url(#mask-2)"
+        mask="url(#transfer-mask)"
         transform="matrix(-1 0 0 1 28 0)"
       />
     </g>
