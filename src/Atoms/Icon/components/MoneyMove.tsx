18653d1635b6d1bb5d9ce41610aba28b0ee5795a
Fredrik Gustafsson <fredrik.gustafsson@nordnet.se>
2021-05-07 14:04:24 +0200

    fix: 🐛 adjust strokes on some icons

--- src/Atoms/Icon/components/MoneyMove.tsx
+++ src/Atoms/Icon/components/MoneyMove.tsx
@@ -11,7 +11,7 @@ export const MoneyMove = (props: BaseProps) => {
           strokeColorFn={props.fill}
           cssAttribute="stroke"
           d="M28 19V9H4m0 4v10h24m-2.828-5.667L28 20.162l2.828-2.829M1.172 14.667L4 11.838l2.828 2.829M16 19a3 3 0 100-6 3 3 0 000 6z"
-          stroke-width="2"
+          strokeWidth="2"
         />
       </g>
     </IconBase>
--- src/Atoms/Icon/components/Stock.tsx
+++ src/Atoms/Icon/components/Stock.tsx
@@ -9,7 +9,7 @@ export const Stock = (props: BaseProps) => {
       <StyledPath
         d="M20.2382 9L22.2382 7H31.6666V16.4277L29.6666 18.4277V10.6302L20.1502 21.3362L10.8666 14.7051L2.12265 25.9473L0.543945 24.7194L10.4667 11.9616L19.8497 18.6638L28.4398 9H20.2382Z"
         strokeColorFn={props.fill}
-        cssAttributes={['stroke', 'fill']}
+        cssAttribute="fill"
       />
     </IconBase>
   );
