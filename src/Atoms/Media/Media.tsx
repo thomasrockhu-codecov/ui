926e81545d4932d472ded53b2f84c14c42c386b9
william bruce <william.bruce@nordnet.se>
2021-06-17 13:08:26 +0200

    fix: restore to styled div outside render

--- src/Atoms/Media/Media.tsx
+++ src/Atoms/Media/Media.tsx
@@ -7,14 +7,11 @@ import { Theme } from '../../theme/theme.types';
 
 const negateMedia = R.pipe(R.split(' '), R.insert(1, 'not all and'), R.join(' '));
 
-const StyleAs = (As: keyof JSX.IntrinsicElements | React.ComponentType<any>) => styled(As)<{
-  query: Props['query'];
-}>`
+const StyledDiv = styled.div<{ query: Props['query'] }>`
   ${(p) => negateMedia(typeof p.query === 'string' ? p.query : p.query(p.theme))} {
     display: none;
   }
 `;
-
 const useMedia = (query: string | ((t: Theme) => string)) => {
   const theme = useContext(ThemeContext);
   const [matches, setMatches] = useState<boolean | null>(null);
@@ -45,11 +42,10 @@ const useMedia = (query: string | ((t: Theme) => string)) => {
 
 const Media: React.FunctionComponent<Props> = (props) => {
   const As = props.as || 'div';
-  const StyledAs = StyleAs(As);
   const matches = useMedia(props.query);
   // If matches === null it means we are in SSR
   // Show css fallback then
-  if (matches === null) return <StyledAs {...props} />;
+  if (matches === null) return <StyledDiv {...props} />;
   return matches ? <As>{props.children}</As> : null;
 };
 
