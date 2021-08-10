d37d871993cda7f40c8ebec26a25f07e25abd5df
John Lindahl <john@johnphoto.se>
2021-08-10 09:57:10 +0200

    fix: storybook now respects darkmode with bg colours

--- .storybook/ThemeDecorator.tsx
+++ .storybook/ThemeDecorator.tsx
@@ -1,9 +1,10 @@
 import React, { ReactNode } from 'react';
 import { StoryFn } from '@storybook/addons';
 import { ThemeProvider } from 'styled-components';
-import { createTheme } from '../src';
 import { DocsContainer } from '@storybook/addon-docs/blocks';
 import { useDarkMode } from 'storybook-dark-mode';
+import { createGlobalStyle } from 'styled-components';
+import { createTheme } from '../src';
 
 const themes = {};
 
@@ -15,11 +16,34 @@ const getTheme = (darkMode) => {
   return themes[key];
 };
 
-const theme = createTheme({ a11yColors: true, darkColors: true });
+const GlobalStyle = createGlobalStyle`
+body {
+    margin: 0;
+    background: ${(p) => p.theme.color.backgroundInput};
+}
 
-export const ThemeDecorator = (storyFn: StoryFn<ReactNode>) => (
-  <ThemeProvider theme={getTheme(useDarkMode())}>{storyFn()}</ThemeProvider>
-);
+#root {
+    background:  ${(p) => p.theme.color.background};
+    position: absolute;
+    top: 10px;
+    left: 10px;
+    right: 10px;
+}
+
+.sbdocs.sbdocs-content {
+    max-width: 1440px;
+}
+`;
+
+export const ThemeDecorator = (storyFn: StoryFn<ReactNode>) => {
+  const theme = getTheme(useDarkMode());
+  return (
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      {storyFn()}
+    </ThemeProvider>
+  );
+};
 
 export const DocsWrapper = ({ children, ...rest }: any) => (
   <DocsContainer {...rest}>
