2c8fb874793d1e59769a7d6cba5a3ca607103690
Fredrik Forsmo <fredrik.forsmo@gmail.com>
2021-08-24 16:50:43 +0200

    chore: storyshots config

--- jest.config.js
+++ jest.config.js
@@ -5,14 +5,11 @@ module.exports = {
   testPathIgnorePatterns: ['/dist/', '/node_modules/'],
   testEnvironment: 'jsdom',
   moduleFileExtensions: ['js', 'ts', 'tsx', 'json', 'md'],
-  transformIgnorePatterns: [
-    'node_modules/(?!(@statecharts|@storybook|react-syntax-highlighter)/.*)',
-  ],
+  transformIgnorePatterns: ['node_modules/(?!(@statecharts|react-syntax-highlighter)/.*)'],
   moduleNameMapper: {
     '\\.(mdx?)$': '<rootDir>/src/mdMock.ts',
   },
   transform: {
-    '^.+\\.stories\\.tsx?$': '@storybook/addon-storyshots/injectFileName',
     '^.+\\.[tj]sx?$': 'babel-jest',
     '^.+\\.mdx?$': '@storybook/addon-docs/jest-transform-mdx',
   },
