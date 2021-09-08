badc45b6fcb0b03fd12fb13d4a7e02c50474d11b
Jesper Annebäck <jesper.anneback@nordnet.se>
2021-09-08 11:52:42 +0200

    fix: 🐛 make UrgentMessage to fix to viewBox

--- src/Atoms/Icon/components/UrgentMessage.tsx
+++ src/Atoms/Icon/components/UrgentMessage.tsx
@@ -12,15 +12,15 @@ const StyledPath = styled.path<StyledChildProps>`
 
 export const UrgentMessage = ({ fill, stroke, ...props }: ChildProps) => {
   return (
-    <IconBase {...props} viewBox="0 0 64 64">
+    <IconBase {...props} viewBox="0 0 89 65">
       <g fill="none" fillRule="evenodd" clipRule="evenodd">
         <StyledPath
-          strokeColorFn={stroke || ((t) => getColor(t, t.color.text))}
-          d="M11 18h29v2H14.72L32 34.688l9.56-8.127 1.42 1.419-4.59 3.901L51 42.81V30h2v16H11V18zm2 24.81l12.61-10.929L13 21.162V42.81zm14.152-9.618L14.681 44h34.638L36.85 33.192 32 37.312l-4.848-4.12z"
+          strokeColorFn={fill || ((t) => getColor(t, t.color.text))}
+          d="M.625 11.75H55v3.75H7.601L40 43.04l17.927-15.238 2.66 2.66-8.606 7.315L75.625 58.27V34.25h3.75v30H.625v-52.5zm3.75 46.519l23.644-20.492L4.375 17.68v40.589zM30.91 40.234L7.526 60.5h64.948L49.09 40.234 40 47.961l-9.09-7.727z"
         />
         <StyledPath
-          strokeColorFn={fill || ((t) => getColor(t, t.color.negative))}
-          d="M53.414 12h-6.828L42 16.586v6.828L46.586 28h6.828L58 23.414v-6.828L53.414 12zM49 16v5h2v-5h-2zm0 8v-2h2v2h-2z"
+          strokeColorFn={stroke || ((t) => getColor(t, t.color.negative))}
+          d="M80.152.5H67.348L58.75 9.098v12.804l8.598 8.598h12.804l8.598-8.598V9.098L80.152.5zM71.875 8v9.375h3.75V8h-3.75zm0 15v-3.75h3.75V23h-3.75z"
         />
       </g>
     </IconBase>
