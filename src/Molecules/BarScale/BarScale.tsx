684e1410e1a382683accd451748e28706d14ee70
Jesper Alvelid <jesper.alvelid@nordnet.se>
2021-09-09 11:20:11 +0200

    fix: make BarScale more flexible in size

--- src/Molecules/BarScale/BarScale.tsx
+++ src/Molecules/BarScale/BarScale.tsx
@@ -6,24 +6,30 @@ import { BarProps, Props } from './BarScale.types';
 import { Box, Flexbox } from '../..';
 
 const Indicator = styled(Box).withConfig({
-  shouldForwardProp: (prop) => !['isActive'].includes(prop),
+  shouldForwardProp: (prop) => !['isActive', 'barHeight'].includes(prop),
 })<BarProps>`
-  height: ${({ theme }) => theme.spacing.unit(3)}px;
+  height: ${(p) => p.theme.spacing.unit(p.barHeight)}px;
   background: ${(p) =>
     p.isActive ? p.theme.color.barScaleActiveBar : p.theme.color.barScaleInactiveBar};
 `;
 
-export const BarScale: React.FC<Props> = ({ value = 0, max = 7, axisLabel = null }) => {
+export const BarScale: React.FC<Props> = ({
+  value = 0,
+  max = 7,
+  axisLabel = null,
+  barHeight = 3,
+  gutter = 1,
+}) => {
   const verifiedMaxRating = isNumber(max) ? max : 7;
   const clampedValue = isNumber(value) ? Math.max(0, Math.min(value, max)) : 0;
   const isActive = (bar: number) => bar <= clampedValue;
 
   return (
     <>
-      <Flexbox container gutter={1}>
+      <Flexbox container gutter={gutter}>
         {R.range(1, verifiedMaxRating + 1)?.map((bar) => (
           <Flexbox key={bar} item flex="1 1 auto">
-            <Indicator isActive={isActive(bar)} />
+            <Indicator isActive={isActive(bar)} barHeight={barHeight} />
           </Flexbox>
         ))}
       </Flexbox>
