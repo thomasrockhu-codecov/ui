fb570472811ab99b4b4a46448c78b49ad00c37d5
fredriklu <fredde_lutz@hotmail.com>
2021-10-13 10:36:55 +0200

    fix: update image icon to not be a copy of fact sheet

--- src/Atoms/Icon/file/image/Image16.tsx
+++ src/Atoms/Icon/file/image/Image16.tsx
@@ -6,8 +6,13 @@ import { IconProps } from '../../IconBase.types';
 const FactSheet16: React.FC<IconProps> = (props) => {
   return (
     <IconBase {...props} width={16} height={16}>
-      <path d="M15 0H1v16h2V2h10v14h2V0z" fill="currentColor" />
-      <path d="M8 6h3V4H8v2zM5 12v-2h6v2H5zM11 14v2H5v-2h6z" fill="currentColor" />
+      <path d="M7 5.5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0Z" fill="currentColor" />
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M10.41 0H0v16h14V3.59L10.41 0ZM2 9.13V2h7.59L12 4.41v4.5l-2.75-2.2-3.8 3.05-2.2-1.46L2 9.13Zm0 2.4V14h10v-2.52l-2.75-2.2-3.7 2.96-2.3-1.54-1.25.84Z"
+        fill="currentColor"
+      />
     </IconBase>
   );
 };
--- src/Atoms/Icon/file/image/Image24.tsx
+++ src/Atoms/Icon/file/image/Image24.tsx
@@ -6,11 +6,16 @@ import { IconProps } from '../../IconBase.types';
 const FactSheet24: React.FC<IconProps> = (props) => {
   return (
     <IconBase {...props} width={24} height={24}>
-      <path d="M17 7h-4V5h4v2zM17 19H7v-2h10v2zM17 15H7v-2h10v2z" fill="currentColor" />
       <path
         fillRule="evenodd"
         clipRule="evenodd"
-        d="M3 1h18v22H3V1zm2 20h14V3H5v18z"
+        d="M4 8a3 3 0 1 1 6 0 3 3 0 0 1-6 0Zm3-1a1 1 0 1 0 0 2 1 1 0 0 0 0-2Z"
+        fill="currentColor"
+      />
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M13.41 0H0v22h18V4.59L13.41 0ZM2 14.13V2h10.59L16 5.41v8.5l-4-3.2-5.05 4.05L4 12.8l-2 1.33Zm0 2.4V20h14v-3.52l-4-3.2-4.95 3.96L4 15.2l-2 1.34Z"
         fill="currentColor"
       />
     </IconBase>
