f025cd2903645991a58bf099a4ecbca78db484d9
Killian de Buitleir <killian.de.buitleir@nordnet.se>
2021-11-24 12:18:44 +0100

    fix: update storyshots

--- src/Molecules/AccordionItem/AccordionItem.tsx
+++ src/Molecules/AccordionItem/AccordionItem.tsx
@@ -1,8 +1,8 @@
 import React, { useState } from 'react';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import { AnimatePresence, motion } from 'framer-motion';
 
-import { Box, OldIcon, Typography } from '../..';
+import { Box, OldIcon, Typography, Icon } from '../..';
 import { isBoolean, isFunction, isString } from '../../common/utils';
 import { Props } from './AccordionItem.types';
 
@@ -42,27 +42,39 @@ const Item = styled.div<{
     !$disableBackgroundColor && $hasFocus ? `${theme.color.background}` : 'transparent'};
 `;
 
-const Button = styled.button<{ $withChevron?: boolean; $disabled?: boolean }>`
+const Button = styled.button<{ withChevron?: boolean; $disabled?: boolean }>`
   font: inherit;
   box-sizing: border-box;
   position: relative;
   display: flex;
+  align-items: center;
   width: 100%;
   border: 0;
   padding-top: ${(p) => p.theme.spacing.unit(3)}px;
   padding-bottom: ${(p) => p.theme.spacing.unit(3)}px;
-  padding-right: ${(p) => p.theme.spacing.unit(p.$withChevron ? 6 : 0)}px;
-  padding-left: ${(p) => p.theme.spacing.unit(p.$withChevron ? 0 : 6)}px;
+  padding-left: 0;
   color: ${(p) => (p.$disabled ? p.theme.color.disabledText : '')};
   background-color: transparent;
   cursor: ${(p) => (p.$disabled ? 'default' : 'pointer')};
   text-align: start;
+  ${({ withChevron, theme }) =>
+    withChevron &&
+    css`
+      padding: ${theme.spacing.unit(3)}px ${theme.spacing.unit(6)}px;
+      justify-content: space-between;
+    `}
 `;
 
-const OldIconWrapper = styled.div<{ $withChevron?: boolean }>`
-  position: absolute;
-  top: 15px;
-  ${(p) => (p.$withChevron ? 'right' : 'left')}: 0;
+const IconWrapper = styled.div<{ withChevron?: boolean }>`
+  order: ${(p) => (p.withChevron ? '1' : '-1')};
+  padding-left: ${(p) => (p.withChevron ? p.theme.spacing.unit(3) : 0)}px;
+  padding-right: ${(p) => (!p.withChevron ? p.theme.spacing.unit(3) : 0)}px; ;
+`;
+
+const MovingChevron = styled(Icon.ChevronUp8)<{ disabled?: boolean; expanded: boolean }>`
+  color: ${(p) => (p.disabled ? p.theme.color.disabledText : '')};
+  transform: rotate(${(p) => (p.expanded ? '0' : '180')}deg);
+  transition: transform 0.16s ease-out;
 `;
 
 const Content = styled(Box)``;
@@ -115,23 +127,16 @@ export const AccordionItem = React.forwardRef<HTMLButtonElement, Props>(
     };
 
     const icon = (
-      <OldIconWrapper $withChevron={withChevron}>
+      <IconWrapper withChevron={withChevron}>
         {(() => {
-          if (withChevron)
-            return (
-              <OldIcon.ThinChevron
-                color={(t) => (disabled ? t.color.disabledText : '')}
-                direction={expanded ? 'up' : 'down'}
-                size={4}
-              />
-            );
+          if (withChevron) return <MovingChevron expanded={!!expanded} />;
 
           if (expanded) return <OldIcon.Minus size={3} fill={(t) => t.color.cta} />;
           return (
             <OldIcon.Plus size={3} fill={(t) => (disabled ? t.color.disabledText : t.color.cta)} />
           );
         })()}
-      </OldIconWrapper>
+      </IconWrapper>
     );
 
     return (
@@ -151,7 +156,7 @@ export const AccordionItem = React.forwardRef<HTMLButtonElement, Props>(
       >
         <Typography as={as} type="secondary" weight="bold">
           <Button
-            $withChevron={withChevron}
+            withChevron={withChevron}
             $disabled={disabled}
             type="button"
             onClick={clickHandler}
@@ -160,9 +165,8 @@ export const AccordionItem = React.forwardRef<HTMLButtonElement, Props>(
             onBlur={() => setHasFocus(false)}
             disabled={disabled}
           >
-            {!withChevron && icon}
             {title}
-            {withChevron && icon}
+            {icon}
           </Button>
         </Typography>
         <AnimatePresence initial={false}>
@@ -192,6 +196,10 @@ export const AccordionItem = React.forwardRef<HTMLButtonElement, Props>(
       </Item>
     );
   },
-) as any as React.FC<Props> & { components: typeof components };
+) as React.ForwardRefExoticComponent<Props> & {
+  components: typeof components;
+} as React.FC<Props> & {
+  components: typeof components;
+};
 
 AccordionItem.components = components;
