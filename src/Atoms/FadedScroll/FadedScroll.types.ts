e351677261e20420021768196257e909f87f6048
Jesper Winér <Jesper.Winer@nordnet.se>
2021-12-01 13:32:41 +0100

    fix: sync default fadeHeight

--- src/Atoms/FadedScroll/FadedScroll.types.ts
+++ src/Atoms/FadedScroll/FadedScroll.types.ts
@@ -10,7 +10,7 @@ export type Props = {
   /** You can provide a maxHeight to the component or let this component take all available space of parent and if the content exceeds that height then the scrollbar will appear.
    */
   maxHeight?: string | number;
-  /** @default 10 */
+  /** @default 15 */
   fadeHeight?: number;
   /** ⚠️Warning, you will most likely have double scrollbars on the page which is bad UX. This could still be valid in cases like Modal where the page scrollbar is locked when open,
    * @default false
