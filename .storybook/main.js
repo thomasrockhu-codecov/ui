862670b931bc3e1d75c8342df8b9c16b86ab9be6
David Skog <david.skog@nordnet.se>
2021-12-21 08:38:04 +0100

    feat: wip webpack config

--- .storybook/main.js
+++ .storybook/main.js
@@ -6,6 +6,9 @@ const storiesDir = process.env.STORYBOOK_DIRECTORY
   : srcPath;
 
 module.exports = {
+  core: {
+    builder: 'webpack5',
+  },
   stories: ['../docs/**/*.stories.mdx', path.join(storiesDir, '**/*.stories.@(js|jsx|ts|tsx|mdx)')],
   addons: [
     '@storybook/addon-essentials',
@@ -18,29 +21,40 @@ module.exports = {
   ],
 
   webpackFinal: async (config) => {
-    config.entry.unshift(require.resolve('core-js/es/weak-set'));
-    config.entry.unshift(require.resolve('focus-within-polyfill'));
-    config.module.rules.push({
-      test: /\.(ts|tsx)$/,
-      loaders: [
-        {
-          loader: 'babel-loader',
-          options: {
-            presets: [['react-app', { flow: false, typescript: true }]],
+    return {
+      ...config,
+      entry: [
+        require.resolve('core-js/es/weak-set'),
+        require.resolve('focus-within-polyfill'),
+        ...config.entry,
+      ],
+      module: {
+        ...config.module,
+        rules: [
+          ...config.module.rules,
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: [['react-app', { flow: false, typescript: true }]],
+            },
           },
-        },
-      ],
-    });
-    config.module.rules.push({
-      test: /\.jsx?$/,
-      loader: require.resolve('babel-loader'),
-      include: [
-        path.resolve(__dirname, '..', 'node_modules', 'use-ssr'),
-        path.resolve(__dirname, '..', 'node_modules', 'color'),
-      ],
-    });
-
-    config.resolve.extensions.push('.ts', '.tsx', '.d.ts', '.md', '.mdx');
-    return config;
+          {
+            test: /\.jsx?$/,
+            loader: require.resolve('babel-loader'),
+            include: [
+              path.resolve(__dirname, '..', 'node_modules', 'use-ssr'),
+              path.resolve(__dirname, '..', 'node_modules', 'color'),
+            ],
+          }
+        ],
+        resolve: {
+          ...config.resolve,
+          extensions: [
+            ...config.resolve.extensions,
+            '.ts', '.tsx', '.d.ts', '.md', '.mdx'
+          ]
+        }
+      }
+    }
   },
 };
