10e20d38db0c6b28399e90d08086fdc133f1dc04
william bruce <william.bruce@nordnet.se>
2022-03-17 09:24:31 +0100

    test(AccountBadgeStack): fix stories

--- src/Molecules/Badge/components/AccountBadgeStack/AccountBadgeStack.stories.tsx
+++ src/Molecules/Badge/components/AccountBadgeStack/AccountBadgeStack.stories.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 import { Story } from '@storybook/react';
+import { action } from '@storybook/addon-actions';
 import { AccountBadgeStack } from './AccountBadgeStack';
 import { AccountBadgeStackProps, Item } from './AccountBadgeStack.types';
 import { Display } from '../../../../common/Display';
@@ -159,50 +160,53 @@ Default.story = {
 
 export const InsideButton = () => {
   return (
-    <Display
-      title="Size s"
-      items={[
-        {
-          component: (
-            <Button.Pill size="m" variant="primary">
-              <Badge.Account.Stack items={[...singleArray, ...singleArray]} />
-            </Button.Pill>
-          ),
-          title: '2 badges',
-        },
-        {
-          component: (
-            <div style={{ background: 'white', padding: '12px' }}>
-              <Button.Pill
-                size="m"
-                variant="secondary"
-                icon={<ChevronDown8 color="currentColor" />}
-                iconPlacement="right"
-              >
+    <DisplayContainer>
+      <Display
+        title="Size s"
+        items={[
+          {
+            component: (
+              <Button.Pill size="m" variant="primary">
                 <Badge.Account.Stack items={[...singleArray, ...singleArray]} />
               </Button.Pill>
-            </div>
-          ),
-          title: '2 badges in secondary button',
-        },
-        {
-          component: (
-            <Button.Pill size="m">
-              <Badge.Account.Stack items={threeArray} />
-            </Button.Pill>
-          ),
-          title: '3 badges with carret',
-        },
-        {
-          component: (
-            <Button.Pill size="m">
-              <Badge.Account.Stack items={fiveArray} maxElementsInStack={3} />
-            </Button.Pill>
-          ),
-          title: '3 badges, with truncation',
-        },
-      ]}
-    />
+            ),
+            title: '2 badges',
+          },
+          {
+            component: (
+              <div style={{ background: 'white', padding: '12px' }}>
+                <Button.Pill
+                  onClick={action('Ouch')}
+                  size="m"
+                  variant="secondary"
+                  icon={<ChevronDown8 color="currentColor" />}
+                  iconPlacement="right"
+                >
+                  <Badge.Account.Stack items={[...singleArray, ...singleArray]} />
+                </Button.Pill>
+              </div>
+            ),
+            title: '2 badges in secondary button',
+          },
+          {
+            component: (
+              <Button.Pill size="m">
+                <Badge.Account.Stack items={threeArray} />
+              </Button.Pill>
+            ),
+            title: '3 badges with carret',
+          },
+          {
+            component: (
+              <Button.Pill size="m">
+                <Badge.Account.Stack items={fiveArray} maxElementsInStack={3} />
+              </Button.Pill>
+            ),
+            title: '3 badges, with truncation',
+          },
+        ]}
+      />
+    </DisplayContainer>
   );
 };
 
@@ -212,86 +216,88 @@ InsideButton.story = {
 
 export const OtherVariants = () => {
   return (
-    <Display
-      title="With different colors"
-      items={[
-        {
-          component: (
-            <Badge.Account.Stack
-              badgeSize="m"
-              items={[
-                { label: 'SEB', badgeColor: (t) => t.color.positive },
-                { label: 'HB', badgeColor: (t) => t.color.badgeBackground },
-                { label: 'AVA', badgeColor: (t) => t.color.negative },
-                { label: 'NN', badgeColor: (t) => t.color.cta },
-              ]}
-            />
-          ),
-          title: 'Other banks',
-        },
-        {
-          component: (
-            <Badge.Account.Stack
-              badgeSize="m"
-              items={[
-                {
-                  label: (
-                    <Typography weight="bold" type="caption" color={(t) => t.color.positive}>
-                      TJP
-                    </Typography>
-                  ),
-                },
-                {
-                  label: (
-                    <Typography
-                      weight="bold"
-                      type="caption"
-                      color={(t) => t.color.negativeBlackBackground}
-                    >
-                      TJP
-                    </Typography>
-                  ),
-                },
-                {
-                  label: (
-                    <Typography
-                      weight="bold"
-                      type="tertiary"
-                      color={(t) => t.color.bubbleBackground}
-                    >
-                      TJP
-                    </Typography>
-                  ),
-                },
-                {
-                  label: (
-                    <Typography weight="bold" type="secondary" color={(t) => t.color.buyHover}>
-                      TJP
-                    </Typography>
-                  ),
-                },
-              ]}
-            />
-          ),
-          title: 'Custom label',
-        },
-        {
-          component: (
-            <Badge.Account.Stack
-              badgeSize="m"
-              maxElementsInStack={2}
-              truncationWrapper={({ children }) => (
-                <Typography weight="bold" type="primary" color={(t) => t.color.negative}>
-                  {children}!
-                </Typography>
-              )}
-              items={threeArray}
-            />
-          ),
-          title: 'Custom truncation item',
-        },
-      ]}
-    />
+    <DisplayContainer>
+      <Display
+        title="With different colors"
+        items={[
+          {
+            component: (
+              <Badge.Account.Stack
+                badgeSize="m"
+                items={[
+                  { label: 'SEB', badgeColor: (t) => t.color.positive },
+                  { label: 'HB', badgeColor: (t) => t.color.badgeBackground },
+                  { label: 'AVA', badgeColor: (t) => t.color.negative },
+                  { label: 'NN', badgeColor: (t) => t.color.cta },
+                ]}
+              />
+            ),
+            title: 'Other banks',
+          },
+          {
+            component: (
+              <Badge.Account.Stack
+                badgeSize="m"
+                items={[
+                  {
+                    label: (
+                      <Typography weight="bold" type="caption" color={(t) => t.color.positive}>
+                        TJP
+                      </Typography>
+                    ),
+                  },
+                  {
+                    label: (
+                      <Typography
+                        weight="bold"
+                        type="caption"
+                        color={(t) => t.color.negativeBlackBackground}
+                      >
+                        TJP
+                      </Typography>
+                    ),
+                  },
+                  {
+                    label: (
+                      <Typography
+                        weight="bold"
+                        type="tertiary"
+                        color={(t) => t.color.bubbleBackground}
+                      >
+                        TJP
+                      </Typography>
+                    ),
+                  },
+                  {
+                    label: (
+                      <Typography weight="bold" type="secondary" color={(t) => t.color.buyHover}>
+                        TJP
+                      </Typography>
+                    ),
+                  },
+                ]}
+              />
+            ),
+            title: 'Custom label',
+          },
+          {
+            component: (
+              <Badge.Account.Stack
+                badgeSize="m"
+                maxElementsInStack={2}
+                truncationWrapper={({ children }) => (
+                  <Typography weight="bold" type="primary" color={(t) => t.color.negative}>
+                    {children}!
+                  </Typography>
+                )}
+                items={threeArray}
+              />
+            ),
+            title: 'Custom truncation item',
+          },
+        ]}
+      />
+    </DisplayContainer>
   );
 };
 
